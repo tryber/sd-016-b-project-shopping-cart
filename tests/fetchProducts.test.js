1252b573b12e4d32f1a7c8c8c714e450e826983a
Gian Fritsche <gian.fritsche@betrybe.com>
2021-10-19 16:17:58 -0300

    fetch products

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,29 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts deve ser uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('ao executar fetchProducts a função fetch deve ser chamada', async () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('ao chamar a função fetchProducts, a função fetch é chamada corretamente com o endpoint', () => {
+    const expected =
+      'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(expected);
+  });
+
+  it("ao chamar a função fetchProducts com o argumento 'computador', deve retornar a mesma estrutura de dados de 'computadorSearch'", async () => {
+    const result = await fetchProducts('computador');
+    expect(result[0]).toEqual(computadorSearch.results[0]);
+  });
+
+  it('ao chamar a função fetchProducts sem argumento, deve retornar um erro', async () => {
+    const result = await fetchProducts();
+    expect(result).toEqual(new Error('You must provide an url'));
+  });
 });
