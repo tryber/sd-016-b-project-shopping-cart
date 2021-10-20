58a2222ba7f068fce093b6a6ae1176ff2f097318
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-20 16:56:04 -0300

    feat: create fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,7 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (url) => {
+  if (!url) throw new Error('You must provide an url');
+  const res = await fetch(url);
+  return res.json();
 };
 
 if (typeof module !== 'undefined') {
