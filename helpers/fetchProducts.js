2c96571b459a673d6f95cdbd8c1709fbb5f55410
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-20 15:26:58 -0300

    fix(helpers): make fetchProducts return results
    The function `fetchProducts` was returning the"${\n}"entire json, and we want only the `results`
    field, that contains the products"

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,7 +4,8 @@ const fetchProducts = (QUERY = 'computador') => {
   const queryUrl = `${API_URL}?q=${QUERY}`;
 
   return fetch(queryUrl)
-    .then((response) => response.json());
+    .then((response) => response.json())
+    .then((data) => data.results);
 };
 
 if (typeof module !== 'undefined') {
