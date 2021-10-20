90ed829921e6f66ede47282e46ceb92dfb6774c7
André Melo <adr.smelo@gmail.com>
2021-10-20 20:27:00 -0300

    feat: teste se a função fetch utiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador ao chamar a função fetchProducts com o argumento/parametro 'computador'

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,8 +9,13 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Teste se fetchProducts é uma função', async () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   });
-  it('Teste se fetch foi chamada executando a função fetchProducts com o argumento computador', async () => {
+  it('Teste se fetch foi chamada executando a função fetchProducts com o argumento "computador"', async () => {
     const fetchCalled = await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
+  });
+  it('Teste se a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador" ao chamar a função fetchProducts com o argumento "computador"', async () => {
+    const fetchCalled = await fetchProducts('computador');
+    const url = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 });
