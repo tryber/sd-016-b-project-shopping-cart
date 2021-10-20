8185c044954e73ff003fc45fc260e2532b319757
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 15:50:24 -0300

    feat: implement fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+const fetchProducts = () => ( 
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .catch((error) => console.log(error))
+);
 
 if (typeof module !== 'undefined') {
   module.exports = {
