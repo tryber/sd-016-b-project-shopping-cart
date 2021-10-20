8bd3d16db6041e195aec02528114d866881073ba
Marcio Junior <marcio.mj70@gmail.com>
2021-10-20 16:47:20 -0300

    test(fetchProducts): throw error when not pass param

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -14,4 +14,10 @@ describe('1 - Teste a função fecthProducts', () => {
 
     expect(request).toEqual(computadorSearch)
   })
+
+  it('Should throw an error when there is no param', async () => {
+    await expect(fetchProducts()).rejects.toEqual(
+      new Error('You must provide an url')
+    )
+  })
 });
