eed96f4806346ab4d662f893acb94f1cf1002eb8
lucas <araujo2012@hotmail.com>
2021-10-20 16:52:15 -0300

    Setup do projeto

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
