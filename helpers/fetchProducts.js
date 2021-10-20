6a56ebbc5bd6eb9d06ae7d5559f042eec7ddefdf
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-20 15:06:11 -0300

    feat(requisito 1): implementa funcao fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,7 @@
 const fetchProducts = () => {
-  // seu código aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json())
+    .then((dataJSON) => dataJSON.results);
 };
 
 if (typeof module !== 'undefined') {
