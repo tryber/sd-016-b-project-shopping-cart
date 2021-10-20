722409059b30ed842514e043dc0ff7af02c26d6c
vinisavordelli <vinicius.savordelli@gmail.com>
2021-10-20 17:27:11 -0300

    3 testes criados para fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,8 @@ const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$';
 
 const fetchProducts = async (QUERY) => fetch(`${API_URL}${QUERY}`)
   .then((response) => response.json())
-  .then((data) => data.results);
+  .then((data) => data.results)
+  .catch((error) => console.log('seu erro é: ', error));
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,6 +6,20 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  test('Testa se ao procurar por computador')
-  fail('Teste vazio');
+  it('Testa se fetchProducts é uma função'), () => {
+    expect(typeof fetchProducts).toEqual('function')
+  }
+
+  it('Testa se ao colocar o parametro "computador" fetch é chamado'), async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  }
+
+  it('Testa se o retorno da função com o parametro "computador" retorna o restudado da constante computadorSearch'), async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetchProducts('computador').toEqual(computadorSearch));
+  }
+
 });
