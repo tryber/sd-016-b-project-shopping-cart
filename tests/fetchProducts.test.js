dd6a371362b0db4dcf49946c540f8c69baaf1ee9
Victor <victor.hugofla@hotmail.com>
2021-10-20 16:52:04 -0300

    Finaliza requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,8 +6,15 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  it('Testa se fetchProducts é uma função', () => {
+  it('Verifica se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
+  })
+  it('Verifica se ao chamar fetchProducts com o argumento computador a função fetch é chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Verifica se ao chamar fetchProducts com o argumento computador a função fetch utiliza o endpoint correto', () => {
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   })
   it('Verifica se retorna o objeto corretamente ao chamar com o argumento computador', async () => {
     expect(await fetchProducts('computador')).toEqual(computadorSearch);
