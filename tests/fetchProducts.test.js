e9727ddaf43d2b4421f7f3220958be595108cd39
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 17:46:50 -0300

    Test: verify if fetchProducts is a function

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,9 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('testa se fetchProducts e uma funcao', async () => {
+    expect.assertions(2);
+    await expect(fetchProducts).toBeDefined();
+    await expect(typeof fetchProducts).toBe('function');
+  })
 });
