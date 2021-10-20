1ea493dc9bbccd99d0d2bbe1d973d0bc19d390bd
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-20 15:20:48 -0300

    feat(helpers): implement fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search';
+
+const fetchProducts = (QUERY = 'computador') => {
+  const queryUrl = `${API_URL}?q=${QUERY}`;
+
+  return fetch(queryUrl)
+    .then((response) => response.json());
 };
 
 if (typeof module !== 'undefined') {
