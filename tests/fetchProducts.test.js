3db2c3be8f5a08e0763f4e366fdadea6ba4897e2
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-20 16:31:49 -0300

    test(fetchProducts): fetchProducts calls fetch

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,4 +8,10 @@ describe('1 - Teste a função fecthProducts', () => {
   test('fetchProducts é uma função', () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   });
+
+  test('fetchProducts deve chamar a função fetch quando executada com o argumento "computador"', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
