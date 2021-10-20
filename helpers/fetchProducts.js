81b39b96a86f24c1e61cd1095582c18d15130a29
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-20 18:06:09 -0300

    lint:

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,6 @@
-const fetchProducts = (q = 'computador') => fetch(
-  `https://api.mercadolibre.com/sites/MLB/search?q=${q}`).then((r) => r.json());
+const fetchProducts = (q = 'computador') => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${q}`).then((r) => r.json());
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
