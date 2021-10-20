b331d587e88df28c009b01f72931cfe29f278ec4
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-10-20 18:01:56 -0300

    fix(script): corrigindo erro do codigo loadProductsPage

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,11 +8,11 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Teste se fetchProducts é uma função', async () =>{
     expect(fetchProducts).toBeInstanceOf(Function); 
   })
-  it('teste se fetch foi chamada quando o parametro computador foi chamado', async () => {
+  it('Teste se fetch foi chamada quando o parametro computador foi chamado', async () => {
     const fetchTest = await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled()
   })
-  it(' Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
     const fetchTest = await fetchProducts('computador');
     const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
     expect(fetch).toHaveBeenCalledWith(url);
