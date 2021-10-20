6f5e529dd3f4d67a874fcdcac207557dbb7f30ae
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-20 17:27:43 -0300

    fetchProducts retorna apenas promise - results

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,5 @@
 const fetchProducts = (q) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${q}}`)
-  .then((r) => r.json());
+  .then((r) => r.json()).then((j) => j.results);
 
 if (typeof module !== 'undefined') {
   module.exports = {
