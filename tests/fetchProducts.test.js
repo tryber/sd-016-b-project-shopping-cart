c8ec79d51246c5495ff2d482fa7dbae0b3c465d6
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 18:38:25 -0300

    Test: Verify fetch Return and Call

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,9 +5,19 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  it('testa se fetchProducts e uma funcao', async () => {
+  it('Testa se fetchProducts e uma funcao', async () => {
     expect.assertions(2);
-    await expect(fetchProducts).toBeDefined();
-    await expect(typeof fetchProducts).toBe('function');
+    const fetchProductsFunction = await fetchProducts
+    expect(fetchProductsFunction).toBeDefined();
+    expect(typeof fetchProductsFunction).toBe('function');
+  })
+  it('Testa se fetch foi chamada quando computador e passado como argumento', async () => {
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Testa se ao chamar a funcao com argumento computador fetch utiliza o endpoint correto', async () => {
+    const correctEndPoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(correctEndPoint)
   })
 });
