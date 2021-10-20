d27be16665053699bd5af38696c4c611cfffd82c
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-20 16:34:12 -0300

    C3 - Requisito 3

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const eT = event.target;
+  eT.remove();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
