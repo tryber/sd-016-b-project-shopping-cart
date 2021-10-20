c24e5beffd2c8f0dc02a9526b3f86319b3f55b75
André Melo <adr.smelo@gmail.com>
2021-10-20 16:36:24 -0300

    feat: implementa a função fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,14 @@
-const fetchProducts = () => {
+const fetchProducts = async (product) => {
   // seu código aqui
-
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.results);
+  // console.log(result);
+  return result;
 };
+
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
