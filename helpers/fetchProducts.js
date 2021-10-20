2f4e6941b37a8c4b576e637431587aea0a1f0e49
vinisavordelli <vinicius.savordelli@gmail.com>
2021-10-20 18:43:45 -0300

    fix nos testes

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,11 @@
-const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$';
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
+// const fetch = require('node-fetch');
 
 const fetchProducts = async (QUERY) => fetch(`${API_URL}${QUERY}`)
   .then((response) => response.json())
-  .then((data) => data.results)
-  .catch((error) => console.log('seu erro é: ', error));
+  .then((data) => data)
+  .catch((error) => console.log('Seu erro é: ', error));
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,20 +6,20 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  it('Testa se fetchProducts é uma função'), () => {
-    expect(typeof fetchProducts).toEqual('function')
-  }
+  it(('Testa se fetchProducts é uma função'), () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
 
-  it('Testa se ao colocar o parametro "computador" fetch é chamado'), async () => {
+  it(('Testa se ao colocar o parametro "computador" fetch é chamado'), async () => {
     expect.assertions(1);
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
-  }
+  });
 
-  it('Testa se o retorno da função com o parametro "computador" retorna o restudado da constante computadorSearch'), async () => {
+  it(('Testa se o retorno da função com o parametro "computador" retorna o restudado da constante computadorSearch'), async () => {
     expect.assertions(1);
-    await fetchProducts('computador');
-    expect(fetchProducts('computador').toEqual(computadorSearch));
-  }
+    const testezinhoLindo = await fetchProducts('computador');
+    expect(testezinhoLindo).toEqual(computadorSearch);
+  })
 
 });
