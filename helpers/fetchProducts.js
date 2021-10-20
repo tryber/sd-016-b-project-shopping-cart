3d4100c5c4ae022ad1f35da9610713d035f0ae85
Izabela <izabelahammer@gmail.com>
2021-10-20 16:04:06 -0300

    Implementacao da funcao fechProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const result = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((data) => data.results);
+  return result;
 };
+
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
