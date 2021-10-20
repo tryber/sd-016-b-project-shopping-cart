70a514c79e142a60a30d290cf461bfcd8889164c
Seu nome <senamatheus.job@gmail.com>
2021-10-20 18:04:34 -0300

    implementação do teste 2

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,4 +8,10 @@ describe('1 - Teste a função fecthProducts', () => {
   test('Teste se fetchProducts é uma função', () => {
     expect(fetchProducts).toBeInstanceOf(Function)
   });
+
+  test('Testar se fetchProduct é executada quando passado o parâmetro "computador"', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
 });
