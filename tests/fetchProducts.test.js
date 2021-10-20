f93e58e7181fe6b41634e0e590e700f73ca408ca
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-20 16:43:55 -0300

    test: implement fetchProducts tests

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,22 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Check fetchProduct is defined', () => {
+    expect(fetchProducts).toBeDefined();
+  });
+
+  it('Check fetchProducts is a function', () => {
+    expect(fetchProducts).toBeInstanceOf(Function)
+  });
+
+  it('Check fetchProducts return expected JSON', async () => {
+    const productsFetched = await fetchProducts('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    expect(productsFetched).toEqual(computadorSearch);
+  });
+
+  it('Check fetchProducts return a error when called without parameters', async () => {
+    const productsFetched = await fetchProducts();
+    expect(productsFetched).toThrowError(new Error('You must provide an url'));
+  });
 });
