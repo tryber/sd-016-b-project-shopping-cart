3e4be9e2f4d14d1d3428d3bd926077ed6d24496e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-20 17:28:36 -0300

    test: implement fetchItem tests

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,20 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Check fetchItem is a function', () => {
+    expect(fetchItem).toBeInstanceOf(Function);
+  });
+
+  it('Check fetchItem calls fetch() function', () => {
+    expect(fetchItem('MLB1615760527')).toBeInstanceOf(Promise);
+  });
+
+  it('Check fetchItem return expected JSON', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  it('Check fetchItem throw errors when called without parameters', () => {
+    expect(() => fetchItem().toThrowError('You must provide an url'));
+  });
 });
