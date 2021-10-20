f1293de251551a1b06e6d7a50ca44eae473176db
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-20 16:56:07 -0300

    Conclui se fetchProducts e uma funcao

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,7 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
 });
