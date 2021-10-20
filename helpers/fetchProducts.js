b9f667a378c70b1aad5691ab88bf35dec5a112c6
Thaís Carrer <carrersimoes@gmail.com>
2021-10-20 16:23:00 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
 const fetchProducts = () => {
-  // seu código aqui
-};
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    const responseF = () => (
+      fetch(url)
+      .then((response) => response.json())
+      .then((data) => data.value)
+      .catch((error) => console.log(error))
+    );
+    return responseF;
+  };
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,8 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Testa se fetchProducts é uma função', async () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
 });
