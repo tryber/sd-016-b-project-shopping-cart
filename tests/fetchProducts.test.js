cd32322e764d20b5f502c5084bac885de5150587
NunesNathan <nunesxd7@hotmail.com>
2021-10-20 20:18:36 -0300

    C2 - adicionando nome aos testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -16,10 +16,10 @@ describe('1 - Teste a função fecthProducts', () => {
     fetchProducts('computador')
     await expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   });
-  test('teste 4', async () => {
+  test('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch;', async () => {
     expect(await fetchProducts('computador')).toEqual(computadorSearch);
   });
-  test('teste 5', async () => {
+  test('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro;', async () => {
     expect(await fetchProducts()).toEqual(new Error('You must provide an url').toString());
   });
 });
