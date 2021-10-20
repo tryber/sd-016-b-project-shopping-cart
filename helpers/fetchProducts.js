b0119a7b1f0deb7eaca5512e5f78cc12c9b184be
vinisavordelli <vinicius.savordelli@gmail.com>
2021-10-20 17:07:16 -0300

    retirado o require(fetch) do fetchProducts.js

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,8 @@
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$';
 
-const fetch = require('node-fetch');
-
 const fetchProducts = async (QUERY) => fetch(`${API_URL}${QUERY}`)
   .then((response) => response.json())
   .then((data) => data.results);
-
-fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
