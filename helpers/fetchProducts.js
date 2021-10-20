ffa37787cbfc0c8f009f29cb1b9a41cb6da71361
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-20 17:36:50 -0300

    fetchProducts retorna o objeto corretamente

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
+const fetchProducts = (product) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 };
 
 if (typeof module !== 'undefined') {
