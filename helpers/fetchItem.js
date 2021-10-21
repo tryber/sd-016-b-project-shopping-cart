d9625214032fb0482688b0e65fdda7cef67b844e
Marcio Junior <marcio.mj70@gmail.com>
2021-10-21 16:41:35 -0300

    feat: fetch items function

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,8 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  const rawData = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
+  const data = await rawData.json();
+
+  return data;
 };
 
 if (typeof module !== 'undefined') {
