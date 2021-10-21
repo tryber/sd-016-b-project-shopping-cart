df98604e76acf93e8dd24503b94fa7cb9d7ad126
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-10-21 14:45:42 -0300

    fix: fixing problem with storage

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,7 +1,4 @@
-const getSavedCartItems = () => {
-  const oldList = localStorage.getItem('cartItems') || undefined;
-  return oldList;
-};
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = (element) => {
-  localStorage.setItem('cartItems', element);
-};
+const saveCartItems = (element) => localStorage.setItem('cartItems', element);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
