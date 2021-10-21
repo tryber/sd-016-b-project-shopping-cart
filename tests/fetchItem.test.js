11f9e797ea5e8e7ab8457a1585e3a51d2cefcf63
Marcio Junior <marcio.mj70@gmail.com>
2021-10-21 17:33:52 -0300

    test(fetchItem): fetchItem use specific endpoint

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -13,4 +13,9 @@ describe('2 - Teste a função fecthItem', () => {
     await fetchItem('MLB1615760527')
     expect(fetch).toHaveBeenCalled()
   })
+
+  it('Should fetchItem use specific endpoint', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527')
+  })
 });
