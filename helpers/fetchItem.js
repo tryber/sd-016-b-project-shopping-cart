c05a54a6044fb29988ce7baf8c9f8c07142856a2
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-10-21 11:37:49 -0300

    All tests have been done of challenge 2, which, by the way, are exactly the same of the first challenge.

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,7 @@
 const fetchItem = async (id) => {
+  if (id === undefined) {
+    return 'You must provide an url';
+  }
   const search = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const result = await search.json();
   return result;
