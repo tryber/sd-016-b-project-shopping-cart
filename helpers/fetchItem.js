ea007b13d72a4fac62c6d65f9a65f6164e06a2ac
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-21 20:24:27 -0300

    fix(fetchItem): fetchItem throws an err when called with 0 args

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,8 @@
 const fetchItem = (id) => {
+  if (!id) throw new Error('You must provide an url');
+
   const queryUrl = `https://api.mercadolibre.com/items/${id}`;
+
   return fetch(queryUrl)
     .then((response) => response.json());
 };
