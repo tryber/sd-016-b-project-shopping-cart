9fcdd5cadf09a3c10695036d2b7ee92fff35f6e9
Matheus <matheusmoura2110@gmail.com>
2021-10-20 22:36:51 -0300

    task 2 e 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const getItens = document.querySelector('.items');
+const getOl = document.querySelector('.cart__items');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -7,8 +8,29 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  getOl.removeChild(event.target);
+  saveCartItems();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const funnn = async (e) => {
+  const lalala = e.target.parentElement.firstChild.innerText;
+  await fetchItem(lalala)
+    .then((reponse) => getOl.appendChild(createCartItemElement(reponse)));
+  saveCartItems(getOl.innerHTML);
+};
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
+  if (element === 'button') e.addEventListener('click', funnn);
   e.className = className;
   e.innerText = innerText;
   return e;
@@ -30,18 +52,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 const constructorHTML = (items) => {
   items.forEach((item) => {
     getItens.appendChild(createProductItemElement(item));
@@ -53,6 +63,7 @@ const constructor = (data) => {
     sku: id,
     name: title,
     image: thumbnail,
+    salePrice: price,
   }));
   constructorHTML(resultObj);
 };
@@ -60,4 +71,4 @@ const constructor = (data) => {
 fetchProducts('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((data) => constructor(data.results));
 
-window.onload = () => { };
+window.onload = () => { getOl.innerHTML = getSavedCartItems(); };
