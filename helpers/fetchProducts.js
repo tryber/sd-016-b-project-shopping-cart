3869d0b256c0e0c91af8350fd49e9863483bb651
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-21 14:43:02 -0300

    fix(fetchProducts): remove API_URL const
    If created in this file, the const API_URL will be available on all scripts
    that are imported after fetchProducts.js on index.html.
    To avoid any problem with API_URL being "global", I decided to include
    the api url directly into the function.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,7 @@
-const API_URL = 'https://api.mercadolibre.com/sites/MLB/search';
-
 const fetchProducts = async (query) => {
   if (!query) throw new Error('You must provide an url');
 
-  const queryUrl = `${API_URL}?q=${query}`;
+  const queryUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 
   return fetch(queryUrl)
     .then((response) => response.json())
