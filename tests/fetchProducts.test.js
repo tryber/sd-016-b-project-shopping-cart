d76dbd44ba9853c35582b438cdde57dc55801697
Eglany Junior <contas.eglanyjr@gmail.com>
2021-10-21 15:10:25 -0400

    test:function use expect endpoint

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,8 +6,17 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  it('test if fecthProducts its a function', () => {
+  it('fecthProducts its a function', () => {
     expect(typeof fetchProducts).toBe('function');
+  })
+  it('function "fetchProducts" heve been called', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Called function fetchProducts, use expect endpoint', () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    fetchProducts('compudator');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   })
   // fail('Teste vazio');
 });
