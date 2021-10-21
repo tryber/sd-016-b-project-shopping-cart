8e4589914874599d0a6c75e1f5baff0c0d0eb518
morallys <morallys.juan@gmail.com>
2021-10-21 18:36:50 -0300

    feat(helpers): altera função fetchItem para acessar a API-ML e recuperar dados do produto passado por parâmetro

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
+const fetchItem = async (ItemID) => {
   // seu código aqui
+  // TODO - Ao clicar no botão deve ser realizada uma requisição. Deverá ser consumido o endpoint: "https://api.mercadolibre.com/items/$ItemID"
+
+  const URL_ITEMID = `https://api.mercadolibre.com/items/${ItemID}`;
+
+  const result = await fetch(URL_ITEMID)
+    .then((response) => response.json());
+
+  return result;
 };
 
 if (typeof module !== 'undefined') {
