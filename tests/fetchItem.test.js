dba7e8741cd119e7de2e11ed605a7b43428d400d
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-21 20:14:56 -0300

    test(fetchItem): fetchItem calls fetch

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -8,4 +8,10 @@ describe('2 - Teste a função fecthItem', () => {
   test('fetchItem é uma função', () => {
     expect(fetchItem).toBeInstanceOf(Function);
   });
+
+  test('fetchItem deve chamar a função fetch quando executada com o argumento "MLB1615760527"', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
