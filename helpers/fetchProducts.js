f5f377c03cc365d0f1abe3cd55f59e649ea527ab
Calixto <wellington1081@gmail.com>
2021-10-21 19:03:44 -0300

    Requisito 01 - ok

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,8 @@
 const fetchProducts = (product) => {
-     return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+     const dataResult = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
     .then((data) => data.json())
     .catch((error) => error);
+    return dataResult;
 };
 
 if (typeof module !== 'undefined') {
