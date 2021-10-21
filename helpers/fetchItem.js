c9d7b7224aa9b5e4ed5c60b99c37b702b93a0445
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-21 12:11:03 -0300

    Fix: FetchItem fetchURL

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,13 +2,12 @@ const fetchItem = (itemId) => {
   if (itemId === undefined) {
     throw new Error('You must provide an url');
   }
-
   return fetch(`https://api.mercadolibre.com/items/${itemId}`)
     .then((response) => response.json())
     .then((data) => data)
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
 };
-fetchItem('MLB1341706310');
+
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
