422e4610e59c22feff849bc3493ccebfacd4d9cc
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-21 19:33:23 -0300

    feat(script): add item to cart

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addProductToCart = (sku) => {
+  const cart = document.querySelector('.cart__items');
+  fetchItem(sku)
+    .then(createCartItemElement)
+    .then((item) => cart.appendChild(item));
+};
+
+function createCustomElement(element, className, innerText, callback) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  // Ideia de adicionar o eventListener aqui dada pelo Gabriel Pinheiro @GabrielFMPinheiro
+  if (callback) e.addEventListener(callback.event, callback.func);
   return e;
 }
 
@@ -19,25 +40,16 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button',
+    'item__add',
+    'Adicionar ao carrinho!',
+    { event: 'click', func: () => addProductToCart(sku) }));
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 const loadProducts = () => {
