754c284b6560d30779b511b19700f65b3d770b37
morallys <morallys.juan@gmail.com>
2021-10-20 22:11:30 -0300

    feat(helpers): Adiciona busca a API Mercado Livre

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = async (paramLink) => {
   // seu código aqui
+  const URL_ML = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
+  const teste = await fetch(`${URL_ML}${paramLink}`)
+    .then((result) => result.json())
+    .then((data) => data)
+    .catch((error) => `Erro apresentado: ${error}`);
+
+  return teste;
 };
 
 if (typeof module !== 'undefined') {
