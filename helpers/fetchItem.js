f65c6a616be317b65d06682b5242059769b1a832
Marcio Junior <marcio.mj70@gmail.com>
2021-10-21 17:40:59 -0300

    refactor(fetchItem): add error if not have id

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,8 @@
 const fetchItem = async (ItemID) => {
+  if (!ItemID) {
+    throw new Error('You must provide an id');
+  }
+
   const rawData = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
   const data = await rawData.json();
 
