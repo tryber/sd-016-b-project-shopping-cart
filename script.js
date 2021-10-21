987ed7c0321bef44e60d0b67b96de3c79bddc8f4
Marcos Carvalho <marcosfilhosp@hotmail.com>
2021-10-21 16:36:02 -0300

    Fix Lint

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
-  );
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
 }
 
