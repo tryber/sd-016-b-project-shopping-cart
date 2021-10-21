95f3e2ac3057747317723e53cd6b664f0b830b49
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-21 19:30:14 -0300

    correção de Lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,9 @@
-const fetchProducts = (product) => {
+const fetchProducts = (product) => 
   // seu código aqui
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
     .then((data) => data.json())
     .catch((error) => error);
-};
-
+    
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
