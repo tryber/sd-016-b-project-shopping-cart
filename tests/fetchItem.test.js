535888f0b7527507d4c3c36de0900fd1b5ae619a
Marcio Junior <marcio.mj70@gmail.com>
2021-10-21 17:25:46 -0300

    test(fetchItem): should be a function

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,7 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts should be a function', () => {
+    expect(typeof fetchItem).toBe('function')
+  })
 });
