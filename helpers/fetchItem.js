8ce56674a1c2979b68706d6406d3b17e49076220
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-22 14:59:37 -0300

    Refactor: rafactor functions fetchItem, Products

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,9 @@
-const fetchItem = (itemId) => fetch(`https://api.mercadolibre.com/items/${itemId}`)
+const fetchItem = (itemId) => (
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
     .then((response) => response.json())
     .then((data) => data)
-    .catch(() => new Error('You must provide an url'));
+    .catch(() => new Error('You must provide an url'))
+);
 
 if (typeof module !== 'undefined') {
   module.exports = {
