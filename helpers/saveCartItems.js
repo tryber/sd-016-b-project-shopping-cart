a6462047b1d894c9b7b8c08f9b3e27f549804dcc
Danielle Silva <danielle.luisasilva@gmail.com>
2021-10-22 17:40:38 -0300

    adiciona teste para funcao saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,8 +1,5 @@
 // A função saveCartItems deve salvar os itens do carrinho de compras no localStorage, em uma chave denominada cartItems. Todas as adições e remoções devem ser abordadas para que a lista esteja sempre atualizada.
 
-// item da lista possui classe 'cart__item'
-// chamar a ol (lista)
-
 const saveCartItems = (productAddToCart) => {
   localStorage.setItem('cartItems', productAddToCart);
 };
