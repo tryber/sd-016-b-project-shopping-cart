b68781213a612bbcc703a3484e7594abbd18dc0a
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-10-22 10:41:26 -0300

    all tests of challenge 4 done. Finally!

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,14 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+it('1. Teste se, ao executar `saveCartItems` com o argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` é chamado', () => {
+  const saveList = `<ol><li>Item</li></ol>`
+  const get = saveCartItems(saveList);
+  expect(localStorage.setItem).toHaveBeenCalled();
+});
+
+it("'2. Teste se, ao executar `saveCartItems` com o argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` é chamado com dois parâmetros, sendo o primeiro 'cartItems' e o segundo sendo o valor passado como argumento para `saveCartItems`", () => {
+  expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', `<ol><li>Item</li></ol>`);
+  });
 });
