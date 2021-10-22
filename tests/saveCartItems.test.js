6e5aca9746da1ffbe07c7021e0fd254bbb7142e2
Marcio Junior <marcio.mj70@gmail.com>
2021-10-22 17:02:58 -0300

    test(saveCartItems): should saveCartItems be called with content expected

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -11,4 +11,9 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems()
     expect(localStorage.setItem).toHaveBeenCalled()
   })
+
+  it('Should saveCartItems be called with content expected', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>')
+  })
 });
