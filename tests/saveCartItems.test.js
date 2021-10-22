72f49493491b13c6462cde932e91a4f680a9304e
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-22 14:57:21 -0300

    test(saveCartItems): saveCartItems calls localStorage.setItem with two args

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -11,4 +11,9 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
+
+  test('saveCartItems, quando chamado com o argumento "<ol><li>Item</li></ol>", chama o método localStorage.setItem("cartItems", "<ol><li>Item</li></ol>") ', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
