bbff5de53ba2dd91ba075292abef635c831e08e8
HiagoBriano <hiago.artist@hotmail.com>
2021-10-22 15:41:10 -0300

    requisito 2 feito

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// requisito 1
 fetchProducts('computador').then((dados) => {
   const dado = dados.results;
   
@@ -42,16 +43,26 @@ fetchProducts('computador').then((dados) => {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// requisito 2
+fetchItem('MLB1615760527').then((dados) => {
+  const skuId = dados.price;
+  const nameItem = dados.title;
+  const preco = dados.price;
+  
+  const add = document.querySelector('.cart__items');
+  add.appendChild(createCartItemElement({ skuId, nameItem, preco }));
+});
 
 window.onload = () => { };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,29 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se é uma função', () => {
+    const validacao = typeof(fetchItem);
+    expect(validacao).toBe('function');
+  })
+
+  test('Verifica se fetch está sendo usada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+
+  test('Verifica se fetch está chamando o site certo', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
+
+  test('Verifica se fetchItem retorna um objeto igual ao item', async () => {
+    const resultado = await fetchItem('MLB1615760527');
+    expect(resultado).toEqual(item);
+  })
+
+  test('Aparece um erro se a função fetchItem for chamada sem parametro', async () => {
+    const resultado = await fetchItem();
+    const erroAqui = new Error('You must provide an url');
+    expect(erroAqui).toEqual(resultado);
+  })
 });
