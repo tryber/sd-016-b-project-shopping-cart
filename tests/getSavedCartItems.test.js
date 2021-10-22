01908c8c1dee6beb62bbd2c2c821b70074d0341a
Marcio Junior <marcio.mj70@gmail.com>
2021-10-22 17:06:58 -0300

    test(getSavedCartItems): should localStorage.getItem be called

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,6 +7,8 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should call localStorage.getItem on call getSavedCartItems', () => {
+    getSavedCartItems()
+    expect(localStorage.getItem).toHaveBeenCalled()
+  })
 });
