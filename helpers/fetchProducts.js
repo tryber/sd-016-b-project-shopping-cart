cfe7c26c1b8a07728faea87f3fce29facda5954e
Renata <renatamagno@live.com>
2021-10-22 15:42:20 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const fetchProducts = (product) => 
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+    .then((data) => data.json())
+    .catch((error) => error);
+
+// Todo o requisito 1 foi feito utilizando o vídeo postado no slack, feito pelo instrutor Bernardo. Onde ele resolveu a questão por completo.  
 
 if (typeof module !== 'undefined') {
   module.exports = {
