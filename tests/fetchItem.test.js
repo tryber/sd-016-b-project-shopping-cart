a6ff999b835a3126c805115f93125aa1de4091d9
Matheus <matheusmoura2110@gmail.com>
2021-10-22 18:09:16 -0300

    fixing test

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,7 +6,7 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   const endPointID = ' https://api.mercadolibre.com/items/MLB1615760527'
-  const id = MLB1615760527
+  const id = 'MLB1615760527'
   it('1 - Teste se fetchItem é uma função;', () => {
     const tipo = typeof fetchItem;
     expect(tipo).toBe('function')
@@ -22,7 +22,7 @@ describe('2 - Teste a função fecthItem', () => {
   })
 
   it('4- Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
-    expect(await fetchItem('MLB1615760527')).toEqual(item);
+    expect(await fetchItem(id)).toEqual(item);
   })
 
   it('5- Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error("mensagem esperada aqui") para comparar com o objeto retornado da API.', async () => {
