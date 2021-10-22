add30fc148ac126679c18d510d831783bafb5ffd
MychaelJales <mychael_duarte@hotmail.com>
2021-10-21 23:40:45 -0300

    refatorando para resolver o lint do requisito 4

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,6 +1,19 @@
+const createLi = (index) => {
+    const li = document.createElement('li');
+    li.id = localStorage.key(index);
+    li.className = 'cart__item';
+    li.innerText = localStorage.getItem(localStorage.key(index));
+    li.addEventListener('click', function (event) { 
+      localStorage.removeItem(event.target.id);
+        return event.target.remove(); 
+      });
+    return li;
+};
+
 const getSavedCartItems = () => {
-  for (let index = 0; index < localStorage.length; index += 1) {
-    addItemCart(localStorage.key(index));
+  const productCarts = document.querySelector('.cart__items');
+  for (let i = 0; i < localStorage.length; i += 1) {
+    productCarts.appendChild(createLi(i));
   }
 };
 
