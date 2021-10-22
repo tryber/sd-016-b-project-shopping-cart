07e10e6f7ed7fc9cf3164feaeb20a61383190f25
Breno Tainan <brenotaguiar@gmail.com>
2021-10-21 22:21:25 -0300

    requisito 01: implementacao dos testes da funcao fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,13 +1,19 @@
-const fetchProducts = async (category) => {
+const fetchProducts = (category) => {
+  if (!category) throw new Error('You must provide an url');
+
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${category}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    return `Algo deu errado :( \n ${error}`;
-  }
+  // try {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   return data.results;
+  // } catch (error) {
+  //   throw new Error('You must provide an url');
+  // }
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.results);
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,33 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  const category = 'computador';
+  test('se fetchProducts é uma função', () => {
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
+
+  test('se fetch foi chamada, quando a função fetchProducts("computador") é chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts(category);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('ao chamar a função fetchProducts("computador"), a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    expect.assertions(1);
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${category}`;
+    await fetchProducts(category);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  test('se a função fetchProducts("computador") é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect.assertions(1);
+    const original = await fetchProducts(category);
+    expect(original).toEqual(computadorSearch.results);
+  });
+
+  test('se chamar a função fetchProducts sem argumento, retorna um erro: You must provide an url', () => {
+    expect.assertions(1);
+    expect(() => fetchProducts()).toThrow(new Error('You must provide an url'));
+  });
+
 });
