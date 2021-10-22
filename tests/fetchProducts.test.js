6c25f2efa324a2063bd9ebc3c1230a2c58935b7b
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-10-22 19:07:03 -0300

    ajuste de detalhes no arquivo

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,16 +9,19 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Teste se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function')
   })
+
   it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', () => {
     fetchProducts('computador')
     expect(fetch).toHaveBeenCalled()
   })
+
   it('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
     const endpoint =
-      'https://api.mercadolibre.com/sites/MLB/search?q=computador"'
+      'https://api.mercadolibre.com/sites/MLB/search?q=computador'
     fetchProducts('computador')
     expect(fetch).toHaveBeenCalledWith(endpoint)
   })
+
   it('Teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
     const results = await fetchProducts('computador')
     expect(results).toEqual(computadorSearch)
@@ -27,6 +30,6 @@ describe('1 - Teste a função fecthProducts', () => {
   it("Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.", async () => {
     const expectedError = new Error('You must provide an url')
     const results = await fetchProducts()
-    expect(result).toEqual(expectedError)
+    expect(results).toEqual(expectedError)
   })
 })
