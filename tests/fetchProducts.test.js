c70640b0fb8cda3ed8be09c6e1a93b74bc6164f1
Serafim <guilhermeserafimbonfim@hotmail.com>
2021-10-22 16:51:56 -0300

    resolve requisito um

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,28 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts is function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('fetchProducts have been called', () => {  
+    fetchProducts('computador');  
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('which endpoint has been used', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('structure data, is equal to computadorSearch', async () => {
+    const test = await fetchProducts('computador');
+    expect(test).toEqual(computadorSearch);
+  });
+
+  it('if param of fetchProducts is undefined return error', async () => {
+    const error = new Error('You must provide an url')
+    const test = await fetchProducts();
+    expect(test).toEqual(error);
+  });  
 });
