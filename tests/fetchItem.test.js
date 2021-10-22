b1429781416ae74160bb6987dec6affd227d1b2c
igor <igor.rafael.briano@gmail.com>
2021-10-22 18:21:36 -0300

    test: criado testes para o segundo requisito do projeto

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,7 +4,26 @@ const item = require('../mocks/item');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('2 - Teste a função fetchItem', () => {
+  it('fetchItem deve ser uma funcao', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('ao chamar fetchItem com o argumento MLB1615760527, testa se fetch foi chamado', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('ao chamar fetchItem com o argumento MLB1615760527, verifica o endpoint do fetch', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('testa se o retorno da funcao é um objeto igual a item', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+  it('funcao sem argumento deve retornar um erro', async () => {
+    const expectError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectError);
+  });
 });
