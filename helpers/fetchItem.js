5d7c0718fed40ed821a30761e396608a45d2be9f
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-22 19:03:01 -0300

    Corrige chamada do metodo json

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
+const fetchItem = (productId) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/items/${productId}`;
+  return fetch(url)
+  .then((response) => response.json())
+  .then((dataJSON) => dataJSON);
 };
 
 if (typeof module !== 'undefined') {
