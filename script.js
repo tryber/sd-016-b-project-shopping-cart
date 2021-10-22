d3d6a61802f3fd07ad90a347803867f5a0954f8b
igor <igor.rafael.briano@gmail.com>
2021-10-22 18:11:26 -0300

    feat: adicionadas funcoes creatButton e addToCarrinho com intuito de fazer com que os botoes 'adicionar ao carrinho' funcionem e adicionar o produto clickado ao carrinho de compras respectivamente

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// função que recebe id, nome, imagem e cria uma section para mostrar eles na tela
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -29,8 +30,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
-  
+  // console.log('ola');
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -41,9 +41,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+// Identifica o ID do item pedido, separa os valores necessarios para a funcao createCartItemElement e adiciona o item ao carrinho
+async function addToCarrinho(event) {
+  const selectDOM = document.querySelector('.cart__items');
+  const sectionID = event.target.parentNode.firstElementChild.innerText;
+  const dadosID = await fetchItem(sectionID);
+  const dadosObj = {
+    sku: dadosID.id, 
+    name: dadosID.title, 
+    salePrice: dadosID.base_price,
+  };
+  const newCarrinho = createCartItemElement(dadosObj);
+  selectDOM.appendChild(newCarrinho);
+}
+
+// faz com que os botoes "adicionar ao carrinho" funcionem e redireciona seu funcionamento para a funcao addToCarrinho
+function creatButton(sectionClass) {
+  const button = sectionClass.lastChild.lastChild;
+  button.addEventListener('click', addToCarrinho);
+}
+
+// separa cada item entregue pela API e faz com que ele apareca na tela atravez da funcao createProductItemElement
 async function SearchProducts(product) {
   const searchData = await fetchProducts(product);
-  const sectionID = document.querySelector('.items');
+  const sectionClass = document.querySelector('.items');
   searchData.results.forEach((acc) => {
     const accObject = {
       sku: acc.id,
@@ -51,7 +72,8 @@ async function SearchProducts(product) {
       image: acc.thumbnail,
     };
     const newItem = createProductItemElement(accObject);
-    sectionID.appendChild(newItem);
+    sectionClass.appendChild(newItem);
+    creatButton(sectionClass);
   });
 }
 
