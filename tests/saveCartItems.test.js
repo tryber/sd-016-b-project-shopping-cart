c04848143f5b686f5ec3f6b4576f0b4d0e816f22
morallys <morallys.juan@gmail.com>
2021-10-22 18:41:50 -0300

    feat(helpers): Cria 2 testes para a funçao saveCartItems()

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,24 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  beforeEach(() => {
+    localStorage.clear;
+  });
+
+  const argument = '<ol><li>Item</li></ol>';
+
+  it('Verifica se ao executar com o argumento "<ol><li>Item</li></ol>", localStorage.setItem é chamado.', () => {
+    expect.assertions(1);
+    saveCartItems(argument);
+
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verifica se ao executar com argumento "<ol><li>Item</li></ol>", o método localStorage.setItem é chamado com dois parâmetros, "cartItems" sendo o primeiro', () => {
+    expect.assertions(1);
+    saveCartItems(argument);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', argument);
+
+  });
 });
