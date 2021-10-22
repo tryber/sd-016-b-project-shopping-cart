9ce7dbbb4cb39aae0aaecddb6128f70ced5d9510
Everton-Curti <evercurti@gmail.com>
2021-10-22 13:00:05 -0300

    refatora lint 4  quesito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,8 @@
 const fetchProducts = async (produto) => {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`)
+  const fetchResults = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`)
     .then((data) => data.json())
     .catch((error) => error);
+  return fetchResults;
 };
 
 if (typeof module !== 'undefined') {
