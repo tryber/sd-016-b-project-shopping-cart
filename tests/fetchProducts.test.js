a498508cba3ce500bc482ae9b96d177e3f5ae0bd
Matheus <matheusmoura2110@gmail.com>
2021-10-22 17:19:18 -0300

    começando testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,26 +5,23 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
-  it('Verificar se é uma function', () => {
-    const isFunction = typeof fetchProducts;
-    expect(isFunction).toBe('function');
-  });
-
-  it('Verifica se a função é chamada', () => {
-    expect(fetchProducts(endPoint)).toBeTruthy();
-  });
-
-  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
-    expect(fetchProducts(endPoint).then((data) => data.query === fetchSimulator(endPoint).query)).toBeTruthy();
-  });
-
-  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
-    expect(await fetchProducts(endPoint)).toEqual(computadorSearch);
-  });
-
-  it('x', async () => {
-    await expect(fetchProducts()).rejects.toThrow('You must provide an url');
-  });
+  it('1- Teste se fetchProducts é uma função', () => {
+    const tipoDaFunc = typeof fetchProducts;
+    expect(tipoDaFunc).toBe('function')
+  })
+  it('2- Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('3- Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  })
+  it('4- Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it('5- Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error("mensagem esperada aqui") para comparar com o objeto retornado da API.', async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'))
+  })
 });
