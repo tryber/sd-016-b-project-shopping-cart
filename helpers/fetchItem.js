85e6d1b603c04edb0f93b08f955259c3473c6970
Millene Galetti <mikagaletti1@gmail.com>
2021-10-22 18:56:49 -0300

    [FEAT] Requisito 2- criando funçao fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemID) => {
+  const url = `https://api.mercadolibre.com/items/${itemID}`;
+
+  const idProduct = await fetch(url)
+  .then((response) => response.json())
+  .catch((error) => console.log(error));
+  return idProduct;
 };
 
 if (typeof module !== 'undefined') {
