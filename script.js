3521fc8d62a925421478115ba8882cbc818f4f5a
thauler <thauler98@gmail.com>
2021-10-22 17:57:32 -0300

    Requisito 1 e 2 sem os testes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const olItem = document.querySelector('.cart__items');
+const productItems = document.querySelector('.items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,17 +8,38 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+// 2
+const getSkuFromProductItem = (event) => {
+  const abc = event.target.parentNode.querySelector('span.item__sku').innerText;
+  console.log(abc);
+  fetchItem(abc)
+  .then((data) => olItem.appendChild(createCartItemElement(data)));
+};
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
+  if (element === 'button') {
+    e.addEventListener('click', getSkuFromProductItem);
+  }
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image } = products) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -24,20 +48,9 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// 1
+fetchProducts('computador')
+  .then((data) => data.results
+  .forEach((element) => productItems.appendChild(createProductItemElement(element))));
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { };
+window.onload = { };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,5 +1,4 @@
 const fetchSimulator = require('../mocks/fetchSimulator');
-const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 window.fetch = jest.fn(fetchSimulator);
