95036d1b8671eb8bea46799955791f29b938784c
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-22 14:46:56 -0300

    Fix: Fix fetchItem and Product error

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,12 +1,8 @@
 const fetchItem = (itemId) => {
-  if (itemId === undefined) {
-    throw new Error('You must provide an url');
-  }
-  
   return fetch(`https://api.mercadolibre.com/items/${itemId}`)
     .then((response) => response.json())
     .then((data) => data)
-    .catch((error) => console.log(error));
+    .catch(() => new Error('You must provide an url'));
 };
 
 if (typeof module !== 'undefined') {
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,11 +1,8 @@
 const fetchProducts = (searchParam) => {
-  if (searchParam === undefined) {
-    throw new Error('You must provide an url');
-  }
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchParam}`)
     .then((response) => response.json())
     .then((data) => data.results)
-    .catch((error) => console.log(error));
+    .catch(() => new Error('You must provide an url'));
 };
 
 if (typeof module !== 'undefined') {
