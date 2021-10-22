39b970b22bc7d7779f8086ab91cb3aef235518c8
igor <igor.rafael.briano@gmail.com>
2021-10-22 16:23:08 -0300

    test: adicionado test para verificar se fetchProducts e uma funcao e se ao chamar esta funcao chamamos tambem um fetch

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,7 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('1 - Teste a função fetchProducts', () => {
+  it('fetchProducts deve ser uma funcao', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('ao chamar fetchProducts com o argumento computador, testa se fetch foi chamado', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
