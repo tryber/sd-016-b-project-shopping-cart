9900c9e031d03293cf0789afb8d2922ad3eb7bcc
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-10-22 11:16:28 -0300

    questao 6

--- script.js
+++ script.js
@@ -13,18 +13,19 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-// 3. Remova o item do carrinho de compras ao clicar nele
-// Ao clicar no produto no carrinho de compra, ele deve ser removido da lista.
+
+// // 3. Remova o item do carrinho de compras ao clicar nele
+// // Ao clicar no produto no carrinho de compra, ele deve ser removido da lista.
 
 function cartItemClickListener(event) {
   getOl.removeChild(event.target);
-  saveCartItems(ol.innerHTML);
+  saveCartItems(getOl.innerHTML);
 }
 
-// 4. Carregue o carrinho de compras através do LocalStorage ao iniciar a página
-// Para completar esse requisito, você deve implementar duas funções dentro da pasta helpers: saveCartItems e getSavedCartItems.
-// A função saveCartItems deve salvar os itens do carrinho de compras no localStorage, em uma chave denominada cartItems. Todas as adições e remoções devem ser abordadas para que a lista esteja sempre atualizada.
-// Já a função getSavedCartItems deve recuperar os itens do carrinho de compras do localStorage quando carregamos a página. Após ter implementado com sucesso as funções saveCartItems e getSavedCartItems, você deve utilizá-las dentro do arquivo script.js.
+// // 4. Carregue o carrinho de compras através do LocalStorage ao iniciar a página
+// // Para completar esse requisito, você deve implementar duas funções dentro da pasta helpers: saveCartItems e getSavedCartItems.
+// // A função saveCartItems deve salvar os itens do carrinho de compras no localStorage, em uma chave denominada cartItems. Todas as adições e remoções devem ser abordadas para que a lista esteja sempre atualizada.
+// // Já a função getSavedCartItems deve recuperar os itens do carrinho de compras do localStorage quando carregamos a página. Após ter implementado com sucesso as funções saveCartItems e getSavedCartItems, você deve utilizá-las dentro do arquivo script.js.
 
 const saveCartItemsSelects = () => {
   getOl.innerHTML = getSavedCartItems();
@@ -36,9 +37,9 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// // 2-Preste atenção que o JSON deve conter apenas um item.
-// // Após ter implementado com sucesso a função fetchItem, você deve utilizá-la dentro do arquivo script.js. Não é necessário importá-la, pois o script fetchProducts.js já está na estrutura do arquivo HTML, basta chamá-la no escopo principal do arquivo. A partir dos dados obtidos pela função fetchItem você deve utilizar a função createCartItemElement() para criar os componentes HTML referentes a um item do carrinho.
-// // Adicione o elemento retornado da função createCartItemElement(product) como filho do elemento <ol class="cart__items">.
+// // // 2-Preste atenção que o JSON deve conter apenas um item.
+// // // Após ter implementado com sucesso a função fetchItem, você deve utilizá-la dentro do arquivo script.js. Não é necessário importá-la, pois o script fetchProducts.js já está na estrutura do arquivo HTML, basta chamá-la no escopo principal do arquivo. A partir dos dados obtidos pela função fetchItem você deve utilizar a função createCartItemElement() para criar os componentes HTML referentes a um item do carrinho.
+// // // Adicione o elemento retornado da função createCartItemElement(product) como filho do elemento <ol class="cart__items">.
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
@@ -51,10 +52,9 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 async function carItems(event) {
   const getSku = getSkuFromProductItem(event.target.parentNode);
   const allList = await fetchItem(getSku);
-  const ol = document.querySelector('.cart__items');
   const car = createCartItemElement(allList);
-  ol.appendChild(car);
-  saveCartItems(ol.innerHTML);
+  getOl.appendChild(car);
+  saveCartItems(getOl.innerHTML);
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -66,20 +66,26 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(button);
+    return section;
+  }
 
-  return section;
-}
-// // 1 Após ter implementado com sucesso a função fetchProducts, você deve utilizá-la dentro do arquivo script.js. Não é necessário importá-la, pois o script fetchProducts.js já está na estrutura do arquivo HTML, basta chamá-la no escopo principal do arquivo. A partir dos dados obtidos pela função fetchProducts você deve utilizar a função createProductItemElement(product) para criar os componentes HTML referentes a um produto.
-// // Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
-// // Obs: as variáveis sku, no código fornecido, se referem aos campos id retornados pela API
+// // // 1 Após ter implementado com sucesso a função fetchProducts, você deve utilizá-la dentro do arquivo script.js. Não é necessário importá-la, pois o script fetchProducts.js já está na estrutura do arquivo HTML, basta chamá-la no escopo principal do arquivo. A partir dos dados obtidos pela função fetchProducts você deve utilizar a função createProductItemElement(product) para criar os componentes HTML referentes a um produto.
+// // // Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
+// // // Obs: as variáveis sku, no código fornecido, se referem aos campos id retornados pela API
 const showItems = async () => {
   const catchFetch = await fetchProducts('computador');
   catchFetch.results.forEach((item) => {
     const catchSection = document.querySelector('.items');
-    const createElementFunc = createProductItemElement(item);
-    catchSection.appendChild(createElementFunc);
+    const createElementItem = createProductItemElement(item);
+    catchSection.appendChild(createElementItem);
   });
 };
+
+// 6 - utilizada para limpar o carrinho de compras.
+const emptyCart = document.querySelector('.empty-cart');
+emptyCart.addEventListener('click', () => {
+  getOl.innerHTML = '';
+});
 
 window.onload = () => {
   showItems();
