81ff491b08301f9e26baabed1c71f3a93bbb9048
Fredi Wilkens <frediwilkens@gmail.com>
2021-10-22 13:42:04 -0300

    feat(tests): adiciona testes para a funcionalidade de recuperar/guardar items do carrinho no localStorage

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,7 +1,7 @@
 const getSavedCartItems = () => {
   const list = document.querySelector('.cart__items');
   const localstorageData = localStorage.getItem('cartItems');
-  if (localStorage.length !== 0) {
+  if (localStorage.length !== 0 && list !== null) {
     list.outerHTML = localstorageData;
   }
 };
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,14 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se chamada com o argumento <ol><li>Item</li></ol> o metodo localstorage.setItem é chamado', () => {
+    const test = '<ol><li>Item</li></ol>';
+    saveCartItems(test);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Verifica se chamada com o parâmetro <ol><li>Item</li></ol> o metodo localStorage.setItem é chamado da maneira correta', () => {
+    const test = '<ol><li>Item</li></ol>';
+    saveCartItems(test);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', test);
+  });
 });
