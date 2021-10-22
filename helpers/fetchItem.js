02c63fa4a6a17ea3e1fcc8732d6b396506e6dcbc
Taina <tainnaps@gmail.com>
2021-10-22 18:56:36 -0300

    Requisito 2 - faz fetchItem sem parametro lancar erro

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,8 @@
 const fetchItem = (id) => {
+  if (!id) throw new Error('You must provide an url');
+
   const endpoint = `https://api.mercadolibre.com/items/${id}`;
-  
+
   return fetch(endpoint)
     .then((response) => response.json());
 };
