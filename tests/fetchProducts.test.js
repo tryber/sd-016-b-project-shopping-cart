5a9e6dee26be253e4c0da7e4c1d724241ecaa540
André Melo <adr.smelo@gmail.com>
2021-10-21 22:32:32 -0300

    feat: teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -20,7 +20,7 @@ describe('1 - Teste a função fecthProducts', () => {
   });
   it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
     const fetchCalled = await fetchProducts('computador');
-    expect(fetchCalled).toEqual(computadorSearch);
+    expect(fetchCalled).toEqual(computadorSearch.results);
   });
   it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
     const expectedError = new Error('You must provide an url');
