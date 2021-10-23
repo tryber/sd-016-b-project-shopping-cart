e2c04175fa5c312eac11f83cdd2a617862d116df
tonistorres <sisvenda2011@gmail.com>
2021-10-23 16:22:28 -0300

    refatorando a funcao getSavedCartItems e comentario line by line

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,13 +1,10 @@
-const getSavedCartItems = () => {
-  const captureOl = document.querySelector('.cart__items'); // captuOL é a tag Ol onde estou refereciando 
-  localStorage.getItem('cartItems');
-  document.querySelectorAll('.cart__items').forEach((item) => {
-    item.addEventListener('click', (event) => {
-      event.target.remove();
-      localStorage.setItem('cartItems', captureOl.innerHTML);
-    });
-  });
-};
+// ******************************************************
+// Funcionalidade: De acordo com o requesito essa função 
+// deve ter uma única funcionalidade, que é recuperar itens
+// do carrinho de compras (localStorage) quando carregamos 
+// a página.
+
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 // ********************
 // COMENTANDO FUNÇÃO: 
