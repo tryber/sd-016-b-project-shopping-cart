3464c22d986e22662ce112ca341008beb9405194
Nathalia <nathaliamagi@gmail.com>
2021-10-23 15:34:49 -0300

    Requisito 1- implemento fetchProducts e testes

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image }) { // { sku, name, image } = product (da função fetchProduct)
   const section = document.createElement('section');
   section.className = 'item';
-
+  // sku = id
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -24,20 +24,44 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+/* 
+- Chame a função fetchProducts.js
+- A partir dos dados da fetchProducts.js: 
+  - crie os elementos HTML na função createProductItemElement(product)
+
+  - Essa questão foi realizada com auxílio do vídeo que o Bê nos disponilizou
+    - LINK: https://app.slack.com/client/TMDDFEPFU/CMT2P6CVC/files/F02JERCBK4M
+*/
+async function searchProducts(product) {
+  const search = await fetchProducts(product);
+  const sectionItem = document.querySelector('.items');
+  search.results.forEach((item) => {
+    const itemObject = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    const searchItem = createProductItemElement(itemObject);
+    sectionItem.appendChild(searchItem);
+  });
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { };
+window.onload = () => {
+  searchProducts('computador');
+};
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,28 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Verifica se fetch foi chamada executando fetchProducts com o argumento computador', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se fetchProducts usou o endpoint correto', () => {
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+
+  it('Verifica se o retorno da função fetchProducts é igual ao mock computadorSearch', async () => {
+    const results = await fetchProducts('computador');
+    expect(results).toEqual(computadorSearch);
+  });
+
+  it('Verifica se fetchProduct recebe algum parâmetro', async () => {
+    const expectedError = new Error('You must provide an url');
+    const resultFetch = await fetchProducts();
+    expect(resultFetch).toEqual(expectedError);
+  });
 });
