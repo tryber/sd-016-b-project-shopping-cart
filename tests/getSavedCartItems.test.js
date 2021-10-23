c06d659bdc958e6e78d765815c8459cb70876b7b
Natã Elienai <nata.elienai@gmail.com>
2021-10-23 16:07:46 -0300

    test: adiciona testes para a funcao getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,6 +7,13 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve invocar o método localStorage.getItem', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Deve invocar o método localStorage.getItem com o argumento \'cartItems\'', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
