733447fa4ca2eb3536d6853dc2182d7b860622aa
Taina <tainnaps@gmail.com>
2021-10-23 19:03:49 -0300

    Requisito 4 - implementa testes de saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,16 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  beforeEach(() => {
+    saveCartItems('<ol><li>Item</li></ol>');
+  });
+
+  it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
