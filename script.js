e397f8a2e4549b8ce8ca3bbd004d541c8e359da1
Everton-Curti <evercurti@gmail.com>
2021-10-23 10:27:03 -0300

    refatora quesito 2

--- script.js
+++ script.js
@@ -60,7 +60,8 @@ function capturarId(e) {
 
 const adicionarItemCarrinho = async (id) => {
   const produto = await fetchItem(id);
-  const adicionarProduto = createCartItemElement(produto);
+  const obj = {sku: produto.id, name: produto.title, salePrice: produto.price};
+  const adicionarProduto = createCartItemElement(obj);
   document.getElementsByClassName('cart__items')[0].appendChild(adicionarProduto);
 };
 
