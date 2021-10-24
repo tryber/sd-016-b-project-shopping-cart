65586a4c777570d676c3f888c87e038bc35484c8
tonistorres <sisvenda2011@gmail.com>
2021-10-24 12:49:42 -0300

    Qt-05 Criando Funcoes: fn()-cria elementos contidos section total-price, fn()-cria a secao total-price

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const tagOl = document.querySelector('.cart__items');
+const sectionCarShopping = document.querySelector('.cart');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -6,72 +7,26 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
+
 // ***********************
 // QUESTÃO 03 FRONT END *
-// *****************************************************************************
-// Remove o Elemento que possui eventos Click
-// recebido por  li.addEventListener('click', cartItemClickListener) 
-// Evento foi invocado em: createCartItemElement
-// *****************************************************************************
+// **********************
 function cartItemClickListener(event) {
    const objecto = event.target;
    objecto.remove();
    saveCartItems(tagOl.innerHTML);
 }
-// *******************************************************************************
-// FRONT END - Essa funçã cria e retorna um Elemento HTML 
-// Etendimento: É a Segunda função mais abstrata a primeira
-// esta logo acima createProductImageElement
-// ******************************************************************************** */
-// 1- Primerio temos a função createCustomElement que recebe 
-//    03(três) parâmetro elemet -> que faz referencia ao elemento
-//    HTML que vocẽ irá criar. O segundo parâmetro é o className->
-//    que onde você deverá nomear o nome da classe que irá ser embutida
-//    na tag desse elemento HTML passado por parâmetro de igual modo 
-//    será deverá passar com parâmetro que irá compor o escopo desse 
-//    elemento;
-// 2- Foi criado uma constante e que irá recer o elemento que será 
-//    criado pelo método createElement vale ressaltar que esse elemento
-//    Pode ser qualquer elemento ao gosto do freguês
-// 3- Criado esse elemento e capturado esse elemento por meio da const e
-//    podemos embutir nesse elemento métodos inerentes ao javascript, para
-//    nosso exemplo estamos trabalhando com dois métodos(atributos) que podemos atribuir
-//    no escopo desse elemento: 1º - className e o 2º innerText
-// 4- passo é retornar esse elemento e podemos dizer que temo uma forma 
-//    Customizada para criar elementos dentro do DOM do nosso arquivo HTML
-// ***************************************************************************************       
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-// ********************* 
-// QUESTÃO 06 FRONT   
-// ****************************************************************//
-// Criar Funcionalidade: Botão de limpar todo carrinho de compras // 
-// **************************************************************//
-// 1- Capture o elemento button na estrutura na árvore DOM por meio do método querySelector
-//    passando como referência para essa captura a classe embebida na tag button empty-cart;
-// 2- Passo criar um evento para esse elemento para isso iremos colocar uma escuta click nele, 
-//    logo, iremos utilizar um evento addEventListerner para colocar essa escuta;  
-// 3- Bem se vamos limpar algo implica dizer que tem algo em algum lugar e no nosso projeto 
-//    equivale dizer que são itens dentro do meu carrinho de compras, logo, para lógica que 
-//    estou utilizando essa limpeza deve acontecer em dois momentos: 1º limpar as li(s) na 
-//    árvore DOM e também limpar tudo dentro de local storage. Para isso iremos escrever toda 
-//    essa lógica dentro da função anônima que está dentor do nosso escutados que já está referenciado 
-//    ao button esvaziar carrinho;
-// 4- Para desenvolvermos essa lógica de limpar a tag primeiro passo é pegarmos a referencia de onde essas
-//    li(s) estão acopladas em que nó Pai daí temos que captuar a tag <ol> 
-// 5- Em seguida criamos uma constante dentro da função anonima que faz referência a ol onde o innerHTML
-//    será limpo;
-// 6- limppamos o innerHTML da tagOL;   
-// 7- em seguida setamos o innerHtml limpo e fazemos a substituição da estrutur ano localStorage por meio
-//    da função saveCartItems passando o innerHTML limpo agora local storage ficará limpo;
-// 8- para garantir que o localStorage ficará totalmente limpo das chaves e values utilizei a função
-//    localStorage.clear() que apaga qualquer vestígio de qualquer arquivo no localStorage no java temos 
-//    um assistente que gerencia essa limpeza automaticamente já aqui temos que fazer no braço.
 
+// ********************* 
+// QUESTÃO 06 FRONT //  
+// *******************
 const buttonClearShopping = document.querySelector('.empty-cart');
 buttonClearShopping.addEventListener('click', () => {
   // const tagOl = document.querySelector('.cart__items'); // tag OL
@@ -88,67 +43,50 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 // *****************************************************
 // COMETÁRIO DA FUNÇÃO: Não irei Utilizar essa função //
 // ********************* ******************************
-// 1- Recebe como parâmetro um item;
-// 2- Retorna item pegando por meio do querySelector o texto 
-//    que está na span propriedade item_sku.innerText;
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function getSkuFromProductItem(item) {  
+   // coloque código aqui
+}
+
+ // ***************************************************** 
+// CRIANDO A FUNÇÃO DE CUSTOMIZAÇÃO DE PREÇO TOTAL 
+// ******************************************************
+// Função cria os elementos que estarão contidos na section
+// Preço Total
+function createCustomPriceTotal(element, innerText, idElement) {
+  const e = document.createElement(element);
+  e.innerText = innerText;
+  e.id = idElement;
+  return e;
+}
+
+// ************************************************************
+// Função que irá criar e acoplar o elemento a seção no HTML /
+// **********************************************************
+function createSectionPrice() {
+  const sectionPriceTotal = document.createElement('section');
+  sectionPriceTotal.className = 'total-price';
+  sectionPriceTotal.appendChild(createCustomPriceTotal('span', 'Soma Total R$', 'labelSomatotal'));
+  sectionPriceTotal.appendChild(createCustomPriceTotal('span', 'Price', 'localPrice'));
+  sectionCarShopping.appendChild(sectionPriceTotal);
 }
 
 // **********************
 // QUESTÃO 02 BACK END //
-// ********************************************************************************************************
-// 1- Criei uma função que irá fazer acesso a uma api utilizando a classe de conexão
-//    fetchItem.js aqui temos uma primeira noção  de  orientação  objeto  quando 
-//    importamos  fetchItem para dentro do arquivo index.html que é diferente do
-//    que conheço que essa classe deveria ser instanciada dentro desse arquivo por meio
-//    da instaciação de um objeto do tipo fetchItem, ou seja, 
-//    FetchItem fetch = new  FetchItem() então faria acesso a essa classe e só 
-//    então poderia pega o método de conexão no caso o fetch e fazer meus teste no 
-//    próprio arquivo, porém, o paradigma aqui é um pouco mais abstrato, pois, no caso 
-//    trabalhase com a ideia de que ele esta importado aqui ou existe essse objeto instanciado 
-//    aqui e devemos trabalhar com o conceito de assinatura do método o que aumenta o grau de 
-//    abstração para esse caso pois de fato a fução ou os objetos criados todos serão invocados
-//    no arquivo index.html por meio de links referenciando arquivos scripst.js;
-// 2- Em seguida pesando na ideia de assinatura e que irei fazer o teste dessa assinatura no 
-//    no momento que executar o index.html utilizei uma estrutura de tratamento de erro também
-//    utilizada no java chamada de try catch;
-// 3- Em seguida fiz um acesso por meio da assintaura fetchItem a essa função passando como 
-//    parametro o tipo de dados string com o id do Produto , pois, é o item que esperaremos o 
-//    retorno, de um unico produto que o end Point irá disponibilizar para alimentar nossa
-//    lista do carrinho de compra;
-// 4- Utilizei duas partículas ou palavras reservdas para fazer menção que esse método é assíncrono,
-//    pois, iremos fazer acesso a um servidor que nos servirá de dados hospedado, neste caso vamos 
-//    trabalhar com ideia de promisse, logo, em JavaScript automaticamente introduz-se o conceito 
-//    de assincronidade que de cara traz duas particulas importantes para serem trabalhadas na função
-//    primeira async e a segunda que é o awiat;
-// 5- Em seguida irei descontruir o objeto dataResult usando destructuring e puxando essa estrutura os
-//    valores das variáveis id, name, price e adicionando as variaveis sku, name, salePrice;
-// 6- Em seguida vou pegar a estrutura criada pelo Front-End createCartItemElement({sku,name,salePrice});
-//    que recebe como parametro um objeto contendo três valores onde irei passar os valores desestruturados
-//    e capturados do meu dataResult que irá alimentar essa estrutura no DOM do meu HTML;
-// 7- Agora irei capturar o local onde será acoplado essa estrutura createCartItemElement por meio do método
-//    querySelector referenciando a classe desse objeto;
-// 8- Por fim irei pegar item que é o local onde irei acoplar essa estrutura e utilizar o método appendChild
-//    passando elementChild como filho para essa este item.
-// **********************************************************************************************************
+// **********************
 async function addShoppingCartBackEnd(event) {
     try {
       const idProduto = event.target.parentNode.firstChild.innerText;
       const dataResult = await fetchItem(idProduto);
       const { id: sku, title: name, price: salePrice } = dataResult;
+     // const priceProduct = salePrice;
+     // sumShoppingCart(parseFloat(salePrice));
       const elementChild = createCartItemElement({ sku, name, salePrice });
       const itens = document.querySelector('.cart__items');
       itens.appendChild(elementChild);
-      // Nesse ponto irei fazer do meu localSotorage meu Banco de Dados Improvisado 
-      // Logo preciso invocar o método que está em script js saveCartItems.js e invocar
-      // sua assinatura nesse ponto, pois, ele só será chamado de fato no index html onde
-      // de fato ocorrera a mágia, porém, a lógica pode ficar nesse ponto
-      // saveCartItems(`id: ${dataResult.id} Produto: ${dataResult.title} Preço:${salePrice}`);
-      // console.log(elementChild);
       saveCartItems(itens.innerHTML);
    } catch (error) {
     console.log('Erro Function addShoppingCarBackEnd:', error);
@@ -173,57 +111,7 @@ function createProductItemElement({ sku, name, image }) {
 
 // **********************
 // QUESTÃO 01 BACK END //
-// ***************************************************************************************************************************
-// 1- Criei uma função que irá fazer acesso a uma api utilizando a classe de conexão
-//    fetchProducts.js aqui temos uma primeira noção  de  orientação  objeto  quando 
-//    importamos  fetchProducts para dentro do arquivo index.html que é diferente do
-//    que conheço que essa classe deveria ser instanciada dentro desse arquivo por meio
-//    da instaciação de um objeto do tipo fetchProducts, ou seja, 
-//    FetchProducts fetch = new  FetchProducts() então faria acesso a essa classe e só 
-//    então poderia pega o método de conexão no caso o fetch e fazer meus teste no 
-//    próprio arquivo, porém, o paradigma aqui é um pouco mais abstrato, pois, no caso 
-//    trabalhase com a ideia de que ele esta importado aqui ou existe essse objeto instanciado 
-//    aqui e devemos trabalhar com o conceito de assinatura do método o que aumenta o grau de 
-//    abstração para esse caso pois de fato a fução ou os objetos criados todos serão invocados
-//    no arquivo index.html por meio de links referenciando arquivos scripts.js;
-// 2- Em seguida pesando na ideia de assinatura e que irei fazer o teste dessa assinatura no 
-//    no momento que executar o index.html utilizei uma estrutura de tratamento de erro também
-//    utilizada no java chamada de try catch;
-// 3- Em seguida fiz um acesso por meio da assintaura fetchProducts a essa função passando como 
-//    parametro o tipo de dados string com o nome computador, pois, é o item que esperaremos o 
-//    retorno, de todos os computadores que o end Point irá disponibilizar para alimentar nossa
-//    pagina com dados vindo da API do mercado livre em nosso caso particular;
-// 4- Utilizei duas partículas ou palavras reservdas para fazer menção que esse método é assíncrono,
-//    pois, iremos fazer acesso a um servidor que nos servirá de dados hospedado, neste caso vamos 
-//    trabalhar com ideia de promisse, logo, em JavaScript automaticamente introduz-se o conceito 
-//    de assincronidade que de cara traz duas particulas importantes para serem trabalhadas na função
-//    primeira async e a segunda que é o awiat;
-// 5- await fetchProducts('computador') - nesse trecho temos a promessa de um arquivo json não mais 
-//    com dados brutos mais dados polidos, ou seja, o json ja convertido em objeto javascript para 
-//    ser manipulados que em nosso caso criamos uma constante chamda results que irá receber desse 
-//    end Point um Objeto contendo várias chaves dentro onde a chave que nos interessa é a results
-//    que é um Array de objetos, logo, temos um objeto contendo uma chave results que é um array de 
-//    objetos. Bem para irmos passo a passo nesse cenário primeiro irei desestruturar esse objeto 
-//    que recebi com retorno e vou de cara pegar a chave results dessa forma iremos direto ao ponto
-//    melhor abordagem para isso é usa o destructuring para capturar essa chave results que esta 
-//    contidade dentro desse objeto recebido pelo end point do mercado livre, o código irá ficar; 
-//    da seguinte forma const { results } = await fetchProducts('computador'), criei uma variável com
-//    o nome results só pra saber qual parte do objeto estamos desestruturando para trabalhar com 
-//    com ela;
-// 6- Beleza o resultado é um array de objetos, já que temos um array de objetos aqui nada melhor
-//    que usar uma HOF forEach() para percorrer esses objetos pegando os valores das chaves que nos
-//    interessa;                     
-// 7- Bem peguei o array results e passei a hof forEach nele passando como parâmetro da hof o item
-//    obrigatório e um parâmetro opcional que foi intitulado position . O que observamos aqui de 
-//    importante é que iremos percorrer um array de objetos e ja'desestruturando cada um dos objetos
-//    percorrido pelo arry retirando de cada objeto o valor da chave id,thumbnail,title e atribuindo esse valor a sku
-//    name, image; 
-// 8 - Sobre desistruturaçãoo vídeo maravilhoso:https://www.youtube.com/watch?v=f8a-qwKC5yk;
-// 9 - em seguida uso a função createProductsElement({sku, name, image}); que ira receber das variáveis desestruturadas 
-//     os valores capturados pelo forEch percorrido e seguida capturamos o lugar onde queremos setar esse valor na página 
-//     HTML por meio do dom e por meio do método appendChild fazemos a inserção dos dados capturados exatamente no lugar 
-//     capturado no dom. 
-// ************************************************************************************************************************
+// **********************
 async function backEndCreateProductItem() {
   try {
    const { results } = await fetchProducts('computador');
@@ -238,6 +126,10 @@ async function backEndCreateProductItem() {
  }
 }
 
+// ******************************************* //
+// FRONT END Busncando LocalSotorage          //
+// setando na tagHtml e colocando de remoção //
+// *******************************************
 function load() {
   const recuperaLocalStorage = getSavedCartItems();
   tagOl.innerHTML = recuperaLocalStorage;
@@ -246,4 +138,5 @@ function load() {
 window.onload = () => {
   backEndCreateProductItem();
   load();
-};
+  createSectionPrice();
+  };
