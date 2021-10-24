4f117baae2fdce25d0a2726929589ce49f3efd47
Thiago Silva <thiago.developer.rj@gmail.com>
2021-10-24 17:14:56 -0300

    Requisito 1 - Ajuste Lint

--- script.js
+++ script.js
@@ -31,24 +31,24 @@ const addProduct = async () => {
   products.results.forEach((prod) => {
     const product = createProductItemElement(prod);
     sectionPai.appendChild(product);
-  })
-}
+  });
+};
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 window.onload = () => {
   addProduct();
