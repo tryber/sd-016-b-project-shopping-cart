bce73e2b4e6f98bd5a56a2b4171c58eb4083eeb0
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-24 16:57:14 -0300

    feat(REQ-3): Implementa lógica para func. cartItemClickListener

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const item = event.target;
+  item.parentNode.removeChild(item);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
