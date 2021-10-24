16c11c09b746ecb8b08f06af05162a328c9507e1
Alissandra <alissandraraysa@gmail.com>
2021-10-24 16:08:10 -0300

    requisito 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,25 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('teste se fecthitem é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+  it('teste se com o parametro MLB1615760527, se fecth foi chamado', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('teste se com o parametro MLB1615760527, utiliza o endpoint esperado', () => {
+    fetchItem('MLB1615760527');
+    const endPoint = 'https://api.mercadolibre.com/items/MLB1615760527'
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+  it('teste se o retorno da função é um objeto igual a item',async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+  it('deve retornar um erro',async () => {
+    const callError = new Error ('You must provide an url');
+    const funcResult = await fetchItem();
+    expect(funcResult).toEqual(callError);
+  });
 });
