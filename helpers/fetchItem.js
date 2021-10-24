178a30d502872af53fd5472acb4b267ab24b5b97
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-24 15:32:19 -0300

    feat(REQ-2): Implementa função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,15 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemId) => {
+  const API_URL = `https://api.mercadolibre.com/items/${itemId}`;
+
+  try {
+    const response = await fetch(API_URL);
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 };
+
+// console.log(fetchItem('MLB1341706310'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
