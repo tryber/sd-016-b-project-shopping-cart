feb690f00f76dc4b642dbb3854d48c0b3be67031
Jonatas Passos <jpc080496@gmail.com>
2021-10-24 10:22:42 -0300

    Implementando a funcao FetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+// eslint-disable-next-line import/no-unresolved
+// const fetch = require('node-fetch');
+
+const fetchProducts = () =>
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((data) => data);
 
 if (typeof module !== 'undefined') {
   module.exports = {
