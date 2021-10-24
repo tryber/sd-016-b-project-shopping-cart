87e0c3d2573cb0fad5df5f322061ac978a822cb6
ronnymv <rjmv.dev@gmail.com>
2021-10-23 23:09:53 -0300

    Finaliza flechItem Function

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,7 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (products) => {
+  return fetch(`https://api.mercadolibre.com/items/${products}`)
+  .then((response) => response.json())
+  .then((data) => data);
 };
 
 if (typeof module !== 'undefined') {
