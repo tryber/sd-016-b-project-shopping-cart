d4f3523decc812d33ced858d68f696b5d780b7a2
Seu nome <senamatheus.job@gmail.com>
2021-10-25 14:24:16 -0300

    melhorando a cobertura dos testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -20,8 +20,8 @@ describe('1 - Teste a função fecthProducts', () => {
   })
 
   it('Testar se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () =>  {
-    const oi = await fetchProducts('computador');
-    expect(oi).toEqual(computadorSearch.results);
+    const fetchWithArg = await fetchProducts('computador');
+    expect(fetchWithArg).toEqual(computadorSearch.results);
   })
 
   it('Testar se, ao chamar a função sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
