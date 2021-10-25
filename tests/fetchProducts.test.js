27fe2857e94e6d6bfab96babfd80a6203438e6d6
thauler <thauler98@gmail.com>
2021-10-25 11:13:12 -0300

    Requisito 1 teste done

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,5 +8,22 @@ describe('1 - Teste a função fecthProducts', () => {
   it('FectchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
-  
+  it('Se o argumento for computador de fetchProducts', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('ao chama-la com o argumento, testa se o endpoint é computador', () => {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('se o retorno da função é um objeto igual a computadorSearch', async () => {
+    const results = await fetchProducts('computador');
+    expect(results).toEqual(computadorSearch);
+  });
+  it('deve retornar um erro', async () => {
+    const error = new Error('You must provide an url');
+    const result = await fetchProducts();
+    expect(result).toEqual(error);
+  });
 });
