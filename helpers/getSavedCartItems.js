64ffa7f08e28dd02c743a265fb616b303ca11561
Millene Galetti <mikagaletti1@gmail.com>
2021-10-25 15:46:43 -0300

    [Feat] Requisito 4- implementando funçao saveCartItems

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,7 @@
 const getSavedCartItems = () => localStorage.getItem('cartItems');
 
+// fonte de pesquisa: https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/getItem
+
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
 }
