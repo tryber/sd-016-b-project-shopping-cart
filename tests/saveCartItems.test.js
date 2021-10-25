88be3f8ee47c31a09b9a2154c454c78d1970d0a7
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-25 14:13:09 -0300

    revert(tests/saveCartItems): "test(saveCartItems): saveCartItems does not overwrites, just append to key"
    This reverts commit 536461b86b18940702d7ddfb64da48954f0790a4.

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -2,19 +2,11 @@ const saveCartItems = require('../helpers/saveCartItems');
 
 Object.defineProperty(window, 'localStorage', {
   value: {
-    setItem: jest.fn(function () {
-      this[arguments[0]] = arguments[1];
-    }),
-    removeItem: jest.fn(function () {
-      delete this[arguments[0]];
-    })
+    setItem: jest.fn(),
   },
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  beforeEach(() => {
-    localStorage.removeItem('cartItems');
-  });
   test('saveCartItems chama o método localStorage.setItem quando chamado com o argumento "<ol><li>Item</li></ol>"', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
@@ -23,12 +15,5 @@ describe('4 - Teste a função saveCartItems', () => {
   test('saveCartItems, quando chamado com o argumento "<ol><li>Item</li></ol>", chama o método localStorage.setItem("cartItems", "<ol><li>Item</li></ol>") ', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
-  });
-
-  test('saveCartItems não sobrescreve, apenas adiciona em "cartItems"', () => {
-    saveCartItems('item1');
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'item1');
-    saveCartItems('item2');
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'item1,item2');
   });
 });
