f4e240c0eda07bdb4f276603bd45690275d8a456
tadeo <salaviawtadeo@gmail.com>
2021-10-25 18:20:03 +0100

    feat: resolve exercise number two

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,7 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (sku) => {
+  return fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(response => response.json())
+    .catch(error => error)
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,32 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('should be a function', () => {
+    expect(typeof fetchItem).toBe('function')
+  })
+
+  it('fetch should be called', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled()
+  })
+
+  it('fetch should be called with the correct endpoint', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527'
+    await fetchItem('MLB1615760527')
+
+    expect(fetch).toHaveBeenCalledWith(endpoint)
+  })
+
+  it('should be returned with data equal to "item" variable', async () => {
+    const response = await fetchItem('MLB1615760527')
+
+    expect(response).toEqual(item)
+  })
+
+  it('should be returned an error if no argument has been provide', async () => {
+    const error = new Error('You must provide an url')
+    const response = await fetchItem()
+
+    expect(response).toEqual(error)
+  })
 });
