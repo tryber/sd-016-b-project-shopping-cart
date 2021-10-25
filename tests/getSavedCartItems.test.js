84fbe3b96ffb3bd58e223f51dc610b84b9ca152f
Israel Sant'Anna <israel.santanna@gmail.com>
2021-10-25 14:16:44 -0300

    revert(tests/getSavedCartItems): "test(getSavedCartItems): getSavedCartItems returns an array..."
    ...when localStorage is not empty
    
    This reverts commit bcb5cfd749aefdff34e6d3c3690e76c7aaa85d83.

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -2,15 +2,7 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 Object.defineProperty(window, 'localStorage', {
   value: {
-    setItem: jest.fn(function () {
-      this[arguments[0]] = arguments[1];
-    }),
-    getItem: jest.fn(function () {
-      return this[arguments[0]];
-    }),
-    removeItem: jest.fn(function () {
-      delete this[arguments[0]];
-    }),
+    getItem: jest.fn(),
   },
 });
 
@@ -28,13 +20,5 @@ describe('4 - Teste a função getSavedCartItems', () => {
   test('getSavedCartItems retorna undefined quando não há itens no localStorage', () => {
     const actual = getSavedCartItems();
     expect(actual).toBeUndefined();
-  });
-
-  test('getSavedCartItems retorna um array quando há itens no localStorage', () => {
-    localStorage.setItem('cartItems', 'item1,item2');
-    const actual = getSavedCartItems();
-    expect(actual).toBeInstanceOf(Array);
-    expect(actual).toHaveLength(2);
-    expect(actual).toStrictEqual(['item1', 'item2']);
   });
 });
