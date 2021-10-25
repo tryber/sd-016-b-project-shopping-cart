cde6c8c583246eca4c802f7519d015068ca8192a
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-10-25 15:49:49 -0300

    Add helpers fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,8 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (id) => {
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {
