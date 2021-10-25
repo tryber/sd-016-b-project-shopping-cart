5a8fbf99980cfe88faa098ece8abb9e56192c45b
Millene Galetti <mikagaletti1@gmail.com>
2021-10-25 14:09:17 -0300

    [TEST] Requisito 2- teste 1

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,8 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verificar se fetchItem é uma função', () => {
+    expect(fetchItem).toBeInstanceOf(Function);
+    // Utilizei diferente matcher do teste fetchProducts, para visualizar possibilidades.
+  })
 });
