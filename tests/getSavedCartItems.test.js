9b29c99db337a85c2cb3ad0143e112a9450b1569
thauler <thauler98@gmail.com>
2021-10-25 12:15:09 -0300

    GetSavedCartItem teste fix

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -8,11 +8,11 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função getSavedCartItems', () => {
   it('se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
-    saveCartItems('<ol><li>Item</li></ol>');
+    getSavedCartItems();
     expect(localStorage.getItem).toHaveBeenCalled();
   });
   it('se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
-    saveCartItems('<ol><li>Item</li></ol>');
-    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });
