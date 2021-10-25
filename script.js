5bfb3bafac0b94dbc23ba9f0e7106b89e4052337
lbcamargo94 <lb.camargo94@gmail.com>
2021-10-25 15:24:05 -0300

    Item 5 completed, added comments to code!

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// Variáveis globais.
 const elementItems = document.querySelector('.items');
 const cartItems = document.querySelector('.cart__items');
 const clearItems = document.querySelector('.empty-cart');
 const load = document.querySelector('.loading');
+const totalCart = document.querySelector('.total-price');
+let total = 0;
 
+// Função para limpar o carrinho.
 function clearCart() {
   cartItems.innerHTML = '';
+  totalCart.innerHTML = '0';
   saveCartItems(cartItems);
 }
 clearItems.addEventListener('click', clearCart);
 
+// Função para criar a imagem do produto.
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -16,11 +22,16 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Função para adicionar o evento de click que remove o item do carrinho.
+// Subtrai o preço do item do valor total.
 function cartItemClickListener(event) {
-  event.target.remove();
+  total -= event.target.innerText.split('$')[1];
+  totalCart.innerText = total;
+  cartItems.removeChild(event.target);
   saveCartItems(cartItems);
 }
 
+// Função para criar os itens no html.
 function createCartItemElement({
   id: sku,
   title: name,
@@ -29,24 +40,36 @@ function createCartItemElement({
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-function getSkuFromProductItem(item) {
+// Função para somar os preços.
+const sumTotalPrice = (price) => {
+  total += price;
+  totalCart.innerText = total;
+};
+
+// Função para buscar o ID dos item clicando no botão adicionar carrinho.
+// Salva os itens no localStorage.
+// Cria o item no carrinho.
+// Soma o preço do item no valor total.
+async function getSkuFromProductItem(item) {
   const itemID = item
     .target
     .parentNode
     .querySelector('span.item__sku')
     .innerText;
-  fetchItem(itemID)
+ await fetchItem(itemID)
     .then((element) => {
       cartItems
         .appendChild(createCartItemElement(element));
       saveCartItems(cartItems);
+      sumTotalPrice(element.price);
     });
 }
 
+// Cria o elemento no html para o produto.
+// Adiciona evento de click 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   if (element === 'button') {
@@ -57,6 +80,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Cria  o produto no elemento do html.
 function createProductItemElement({
   id: sku,
   title: name,
@@ -71,20 +95,24 @@ function createProductItemElement({
   return section;
 }
 
-function removeLoadingResponse() { load.remove(); }
+// Remove a descrição de loading após o carregamento dos itens.
+const removeLoadingResponse = () => load.remove();
 
-const solve = () => {
- fetchProducts('computador')
-  .then((response) => {
- response.results
-    .forEach((element) => {
-      elementItems
-        .appendChild(createProductItemElement(element));
-    });  
-    removeLoadingResponse();
-  });  
+// Acessa a API, trata a resposta e adiciona os itens na tela.
+const solveResponse = () => {
+  fetchProducts('computador')
+    .then((response) => {
+      response.results
+        .forEach((element) => {
+          elementItems
+            .appendChild(createProductItemElement(element));
+        });
+      removeLoadingResponse();
+    });
 };
 
+// Recupera o status do carrinho no local storage.
+// Adiciona o evento de click novamente aos itens do carrinho.
 const addEventClick = () => {
   cartItems.innerHTML = getSavedCartItems();
   const itemsShopping = document.querySelectorAll('.cart__items');
@@ -94,6 +122,6 @@ const addEventClick = () => {
 };
 
 window.onload = () => {
-  solve();
+  solveResponse();
   addEventClick();
 };
