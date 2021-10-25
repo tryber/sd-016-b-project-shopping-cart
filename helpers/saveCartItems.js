0f785cd2c192a6e3a0ec4a640c90c0206ca4eb4e
Millene Galetti <mikagaletti1@gmail.com>
2021-10-25 15:48:59 -0300

    [Refactor] Requisito 4- refatorando a funçao saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,5 @@
-const saveCartItems = (item) => {
-  localStorage.setItem('cartItems', item);
-};
+const saveCartItems = (item) => localStorage.setItem('cartItems', item);
+
 // fonte de pesquisa: https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/setItem
 
 if (typeof module !== 'undefined') {
