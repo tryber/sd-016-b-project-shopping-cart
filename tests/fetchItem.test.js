d1320ff4cec5f7a8496718599d25e745a77be995
André Melo <adr.smelo@gmail.com>
2021-10-25 15:02:26 -0300

    feat: teste se fetch foi chamada executando a função fetchItem com o argumento/parametro 'MLB1615760527'

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -9,4 +9,8 @@ describe('2 - Teste a função fecthItem', () => {
   it('Teste se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   });
+  it('Teste se fetch foi chamada executando a função fetchItem com o argumento "MLB1615760527"', async () => {
+    const fetchCalled = await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
