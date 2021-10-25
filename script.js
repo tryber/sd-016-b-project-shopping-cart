59342994b8e1ae9c17930a6b66d2d72cfb81e058
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-25 18:15:37 -0300

    Requisito 3 completo

--- script.js
+++ script.js
@@ -28,8 +28,10 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// Requisito 3
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
