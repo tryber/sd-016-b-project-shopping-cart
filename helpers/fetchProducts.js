30832832d6f75ba42a9b4b80bd27a12ddbdacd8c
Marllon <marllon3636@gmail.com>
2021-10-25 17:20:24 -0300

    Fix lint req. 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,16 +1,13 @@
 const fetchProducts = async (product) => {
-  const api_url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
 
   if (!product) {
     return new Error('You must provide an url');
   }
 
-  const response = await fetch(api_url);
+  const response = await fetch(API_URL);
   return response.json();
-
 };
-
-console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {
