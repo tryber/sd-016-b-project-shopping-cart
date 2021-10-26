d51b480c81fab11f0d5b4e5550e0a4899c3c2d58
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-10-26 19:31:30 -0300

    adiciona a função fetchProducts do requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (product) => {
+  const dataPromise = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+    .then((response) => response.json())
+    .catch((error) => error);
+
+    return dataPromise;
 };
 
 if (typeof module !== 'undefined') {
