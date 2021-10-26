2ce8235aa637fd0ff9976c88fc7cab9e68c9934a
Joey <joviavelino@gmail.com>
2021-10-26 05:14:44 -0300

    requisito 1 complete

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,12 @@
 const fetchProducts = async (query) => {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-
-const result = await fetch(url)
-.then((response) => response.json())
-.catch(() => new Error('You must provide an url'));
-return result;
+  if (typeof query !== typeof '') {
+    return new Error('You must provide an url');
+  }
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+    return result;
 };
 
 if (typeof module !== 'undefined') {
