25a0c874a685874ffbbab241944125f85337805d
Gabriel A <gabrielaug4@gmail.com>
2021-10-26 17:07:36 -0300

    req.05

--- script.js
+++ script.js
@@ -6,6 +6,23 @@
 const itemClass = document.querySelector('.items');
 const cartItemsClass = document.querySelector('.cart__items');
 const emptyCartClass = document.querySelector('.empty-cart');
+const cartClass = document.querySelector('.cart');
+
+const itemsPrice = async () => {
+  const items = await cartItemsClass.childNodes;
+  const totalPriceClass = document.querySelector('.total-price');
+  if (items.length === 0) {
+    totalPriceClass.innerText = 0;
+  }
+  const newArray = [];
+  for (let index = 0; index < items.length; index += 1) {
+    newArray.push(items[index].innerText.split(' '));
+  }
+  const pricesArray = newArray.map((price) => (price[price.length - 1]).substring(1));
+  const numbersArray = pricesArray.map((number) => parseFloat(number));
+  const result = numbersArray.reduce((acc, number) => acc + number);
+  totalPriceClass.innerText = `Total: $${result}`;
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,6 +58,7 @@ function cartItemClickListener(event) {
   const eventTarget = event.target;
   eventTarget.remove();
   saveCartItems();
+  itemsPrice();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -75,6 +93,7 @@ const getCartElement = async (ID) => {
   };
   cartItemsClass.appendChild(createCartItemElement(objOfReturnedItem));
   saveCartItems();
+  itemsPrice();
 };
 
 const addElementToCart = (event) => {
@@ -87,10 +106,20 @@ const deflateCart = () => {
   cartItemsClass.innerHTML = '';
 };
 
+const createTotalPriceClass = () => {
+  const p = document.createElement('p');
+  p.className = 'total-price';
+  p.innerText = 0;
+  cartClass.appendChild(p);
+}
+
 window.onload = () => { 
   getProductsSection();
   itemClass.addEventListener('click', addElementToCart);
   cartItemsClass.addEventListener('click', cartItemClickListener);
+  cartItemsClass.addEventListener('change', itemsPrice);
   emptyCartClass.addEventListener('click', deflateCart);
   getSavedCartItems();
+  createTotalPriceClass();
+  itemsPrice();
 };
