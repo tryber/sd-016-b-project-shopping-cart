f876b3c870dc34d39064710d6548aad087f2b636
André Melo <adr.smelo@gmail.com>
2021-10-26 12:56:17 -0300

    feat: teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro cartItems e o segundo sendo o valor passado como argumento para saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -12,4 +12,8 @@ describe('4 - Teste a função saveCartItems', () => {
     const saveCartItemsCalled = saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    const saveCartItemsCalled = saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
