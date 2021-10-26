2983fd377457c7cda46f8645c2cb0001cf786547
Felipe Pugas <felipepcan@gmail.com>
2021-10-26 16:51:34 -0300

    requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,8 @@
-const fetchItem = () => {
+const fetchItem = (id) => fetch(`https://api.mercadolibre.com/items/${id}`)
   // seu código aqui
-};
+  .then((data) => data.json())
+  .then((item) => item)
+  .catch((error) => error);
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,4 +6,25 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
+  it('is it a funcion', () => {
+    expect(typeof fetchItem,).toBe('function');
+  })
+  it('was fetch called', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('ao chama-la com argumento computador testa se fetch foi chamada com endpoint correto', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  })
+  it('is fetchItem return equal to item', async () => {
+    const results = await fetchItem('MLB1615760527'); 
+    expect(results).toEqual(item);
+  })
+  it('must return an error', async () => {
+    const result = await fetchItem();
+    const expectedError = new Error('You must provide an url');
+    expect(result).toEqual(expectedError);
+  })
 });
