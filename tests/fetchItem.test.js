69ea54702bf66823eea562b7b9c2bd004f4ee87b
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-26 15:51:59 -0300

    [new] saveCartItems.test ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,29 +6,29 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   
-  test('1.Teste se fetchItem é uma função?', () => {
+  test('1. Teste se fetchItem é uma função?', () => {
     expect.assertions(1);
     expect(fetchItem).toBeInstanceOf(Function)
   });
 
-  test('2.Teste se fetchItem com parametro \'MLB1615760527\' chama fetch', async () => {
+  test('2. Teste se fetchItem com parametro \'MLB1615760527\' chama fetch', async () => {
     expect.assertions(1);
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenLastCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
   });
 
-  test('3.Teste se, ao chamar a função fetchItem com o argumento do item \"MLB1615760527\", a função fetch utiliza o endpoint \"https://api.mercadolibre.com/items/MLB1615760527\"', async () => {
+  test('3. Teste se, ao chamar a função fetchItem com o argumento do item \"MLB1615760527\", a função fetch utiliza o endpoint \"https://api.mercadolibre.com/items/MLB1615760527\"', async () => {
     expect.assertions(1);
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenLastCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
   });
 
-  test('4.Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto \"item\" que já está importado no arquivo.', async () => {
+  test('4. Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto \"item\" que já está importado no arquivo.', async () => {
     expect.assertions(1);
     await fetchItem('MLB1615760527').then((c) => expect(c).toEqual(item));
   });
 
-  test('5.Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url.', () => expect(() => fetchItem())
+  test('5. Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url.', () => expect(() => fetchItem())
   .toThrowError(/You must provide an url/));
 
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,15 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  test('1. Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  test('2. Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenLastCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
+
 });
