cc9aad8c19f58fafd71d4b90812b6f99f98be9fe
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-10-26 12:56:48 -0300

    Requisito-02-1

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -9,14 +9,24 @@ describe('2 - Teste a função fecthItem', () => {
     expect(typeof fetchItem).toBe('function');
   });
 
-  it('A endpoint foi usada', () => {
-    fetchItem('MLB1615760527');
-    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
-  });
-
   it('fetchItem foi chamado', () => {  
     fetchItem('MLB1615760527');  
     expect(fetch).toHaveBeenCalled();
   });
 
+  it('A endpoint foi usada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('dados da estrutura é igual ao item', async () => {
+    const test = await fetchItem('MLB1615760527');;
+    expect(test).toEqual(item);
+  });
+
+  it('se o parâmetro de fetchItem for indefinido, devolver erro', async () => {
+    const error = new Error('You must provide an url')
+    const test = await fetchItem();
+    expect(test).toEqual(error);
+  });
 });
