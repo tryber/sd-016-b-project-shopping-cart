f7a61f5753252f5f98e0dfda2382316fcd7bc86b
Giovanni Greco <giovannivpgreco@protonmail.com>
2021-10-26 17:31:14 -0300

    requisito 2 parcial

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,8 @@
-const fetchItem = () => {
+const fetchItem = (id) => {
   // seu código aqui
+  return fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((response) => response.json())
+  .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const cartItems = document.querySelector('.cart__items');
+
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -18,26 +21,41 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const cartBtn = section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  cartBtn.addEventListener('click', addProductCart)
 
   return section;
 }
 
+// returns product ID
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+
+// 
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+// requisito 2 - adds to cart
+const addProductCart = async (event) => {
+  const productId = getSkuFromProductItem(event.target.parentNode);
+  const fetchProduct = await fetchItem(productId);
+  const cart = createCartItemElement(fetchProduct);
+  cartItems.appendChild(cart);
+
+}
+ 
+// requisito 1 - funcao faz fetch da list de produtos a partir de um parametro query e leva ela formatada para o corpo do site
 
 async function searchProducts(query) {
  const fetchedProducts = await fetchProducts(query);
@@ -51,6 +69,16 @@ async function searchProducts(query) {
    const productItem = createProductItemElement(productData);
    productSection.appendChild(productItem);
  });
+}
+
+// requisito 2 -  add to cart:
+
+const addToCart = async (product) => {
+  const productToAdd = await fetchItem(product);
+  const productInfo = {
+    sku: productToAdd.id,
+
+  }
 }
 
 window.onload = () => { searchProducts('computador'); };
