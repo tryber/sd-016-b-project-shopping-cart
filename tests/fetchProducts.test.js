3a70f35f3b512f16e2549df062d13bd539305177
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-10-26 11:20:19 -0300

    Requisito-01

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,18 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('fetchProducts foi chamado com', () => {  
+    fetchProducts('computador');  
+    expect(fetch).toHaveBeenCalled();
+  });
+  
+  it('se o parâmetro de fetchProducts for indefinido, devolver erro', async () => {
+    const error = new Error('You must provide an url')
+    const test = await fetchProducts();
+    expect(test).toEqual(error);
+  });  
 });
