d92d75cdff79e1fbdedfaf282064d3dfc9fd5052
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-26 19:19:45 -0300

    Conclui testes 1 e 2 do requisito 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,5 +6,12 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Se fetch foi chamada ao executar a função `fetchItem` com o argumento "MLB1615760527"', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toBeCalled();
+  });
 });
