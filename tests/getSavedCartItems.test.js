dde5694d707819567984fbf9cb3cb43eee21917f
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-10-26 15:54:40 -0300

    testes de getSavedCartItem

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,12 +1,18 @@
-const getSavedCartItems = require('../helpers/getSavedCartItems');
+const getSavedCartItems = require('../helpers/getSavedCartItems')
 
 Object.defineProperty(window, 'localStorage', {
   value: {
     getItem: jest.fn(),
   },
-});
+})
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
-});
+  it('Teste se, ao executar saveCartItems, o método localStorage.setItem é chamado', () => {
+    getSavedCartItems()
+    expect(localStorage.getItem).toHaveBeenCalled()
+  })
+  it("Verificar se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o 'cartItems' como parâmetro.", () => {
+    getSavedCartItems()
+    expect(localStorage.getItem).toHaveBeenCalled('cartItems')
+  })
+})
