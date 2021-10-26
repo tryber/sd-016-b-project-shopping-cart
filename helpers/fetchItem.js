ee0b38d3c89cb76c74a59f3f32fcb11166daf8d7
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-10-25 22:14:57 -0300

    solves requirement's 2 challenge

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,9 @@
-const fetchItem = () => {
+const fetchItem = (itemID) => 
   // seu código aqui
-};
+  fetch(`https://api.mercadolibre.com/items/${itemID}`)
+  .then((data) => data.json())
+  .then((item) => item)
+  .catch((error) => error);
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -5,11 +5,36 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
+}
+
+// inspirado por Jonhathan Passos.
+async function addToCart(event) {
+  const getID = getSkuFromProductItem(event.target.parentNode);
+  const prod = await fetchItem(getID);
+  const sendToCart = createCartItemElement(prod);
+  const ol = document.querySelector('.cart__items');
+  ol.appendChild(sendToCart);
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -19,25 +44,13 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  // inspirado por Jonhathan Passos.
+  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const makeButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  makeButton.addEventListener('click', addToCart);
+  section.appendChild(makeButton);
 
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 async function searchProduct(product) {
@@ -52,7 +65,7 @@ async function searchProduct(product) {
     const productItem = createProductItemElement(itemObject);
     addItemsToSite.appendChild(productItem);
   });
-} 
+}
 
 window.onload = () => { 
   searchProduct('computador');
