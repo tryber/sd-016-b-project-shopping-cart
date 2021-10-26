3f0cf64d97ac36de29a5cf5622e156558f5c0da2
Thaís Carrer <carrersimoes@gmail.com>
2021-10-26 17:48:18 -0300

    fix lint

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ function clearCartItems() {
   });
 }
 
-
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
