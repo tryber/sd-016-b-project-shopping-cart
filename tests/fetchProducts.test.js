38149462f4820625eb298c890d32d7732c5ac6d7
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-26 19:12:27 -0300

    Conclui teste 3 do requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -10,8 +10,13 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(typeof fetchProducts).toBe('function');
   });
 
-  it('Se fetch foi chamado ao executar `fetchProducts` com o parametro "computador".', () => {
+  it('Se fetch foi chamado ao executar `fetchProducts` com o parâmetro "computador".', () => {
     fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Se fetch usa o endpoint especificado ao executar `fetchProducts` com o parâmetro "computador".', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 });
