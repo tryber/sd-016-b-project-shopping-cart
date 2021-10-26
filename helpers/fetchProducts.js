b830187e3ebea2930840841aa314aa2e88f00dc5
Bruno <blmarques.dev@gmail.com>
2021-10-25 22:11:37 -0300

    Add: test if fetchItems is function

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,9 +2,8 @@ const apiML = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 const fetchProducts = async (query) => {
   if (query) {
     return fetch(`${apiML}${query}`)
-    .then((promise) => promise.json()
-      .then((data) => data))
-    .catch(() => new Error('You must provide an url'));
+      .then((promise) => promise.json()
+        .then((data) => data));
   }
   throw new Error('You must provide an url');
 };
