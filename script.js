d015b79b056c1bd71fbbd4130becfac679d5beb5
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2021-10-26 12:58:40 -0300

    Requisito 5 refatorado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 // Get elements HTML
 const cartItems = document.querySelector('.cart_items');
 const totalPrice = document.querySelector('.total-price');
@@ -12,19 +15,20 @@ const cleanCart = () => {
     localStorage.clear();
   });
 };
+// Img off product
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
@@ -36,24 +40,67 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
-
+};
+// Return id off product
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
+};
+// Save and refresh product on Local Storage
+const saveLs = () => {
+  const product = cartItems.innerHTML;
+  saveCartItems(JSON.stringify(product));
+};
+// When refresh get the value in Local Storage
+const getValueLs = () => totalPrice.innerHTML = localStorage.getItem('valueProduct');
+// subtraindo valor produtos
+const subValues = (event) => {
+  const textItem = event.target.innerText;
+  const arrItem = textItem.split(' ');
+  const arrLengthPosition = arrItem[arrItem.length - 1];
+  const value = arrLengthPosition.replace('$', '');
+  let sub = Number(localStorage.getItem('valueProduct'));
+  sub -= value;
+  localStorage.setItem('valueProduct', s);
+  totalPrice.innerText = sub;
+};
+// Remove product on cvart
 function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+  event.target.remove();
+  subValues(event);
+  saveLs();
+};
+// Fill the cart with Local Stortage
+const getInfos = () => {
+  const products = JSON.parse(getSavedCartItems());
+  cartItems.innerHTML = products;
 
+  const list = document.querySelectorAll('.cart_item');
+  list.forEach((li) => li.addEventListener('click', cartItemClickListener));
+};
+// soma valores
+const sumValues = (price) => {
+  let sum = Number(localStorage.getItem('valueProduct'));
+  sum += price;
+  localStorage.setItem('valueProduct', sum);
+  totalPrice.innerText = sum;
+};
+// Create the product to add cart
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
+};
+// Add product in cart
+const selectedItem = async (event) => {
+  const id = getSkuFromProductItem(event.target.parentNode);
+  const element = await fetchItem(id);
+  const cart = createCartItemElement(element);
+  cartItems.appendChild(cart);
+  saveLs();
+};
+// Create Prod
 const addItems = async (product) => {
   await fetchProducts(product).then((results) => {
     const items = document.querySelector('.items');
