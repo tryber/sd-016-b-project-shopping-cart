041397e5effc893c89f8d297938dfbe9b8767730
ronnymv <rjmv.dev@gmail.com>
2021-10-25 22:32:04 -0300

    Finalize requisit 1 with all tests

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,10 @@
 const fetchProducts = (param) => {
-    if (!param) { return 'You must provide an url'; }
-      const link = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
-      return fetch(link)
-      .then((response) => response.json()
-      .then((data) => data.results));
-    };
+  if (!param) { return 'You must provide an url'; }
+  const link = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+  return fetch(link)
+  .then((response) => response.json()
+  .then((data) => data.results));
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
