653dc95df4d23b439d39d10d0631a76341a2d892
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-10-26 18:19:25 -0300

    fix lint 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,8 @@
-const fetchProducts = (QUERY) => 
+const fetchProducts = (QUERY) =>
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
     .then((response) => response.json())
     .then((data) => data)
     .catch((error) => error);
-
-  
 
 if (typeof module !== 'undefined') {
   module.exports = {
