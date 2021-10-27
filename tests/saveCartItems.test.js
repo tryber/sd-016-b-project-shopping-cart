eee7eb5c48acc8d2d3bf99f6ed7a23a7b03cdfb0
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-10-26 21:22:43 -0300

    Conclui teste 2 do requisito 4

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -12,4 +12,9 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toBeCalled();
   });
+
+  it('Teste se, ao executar saveCartItems com o argumento "<ol><li>Item</li></ol>", o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>')
+  });
 });
