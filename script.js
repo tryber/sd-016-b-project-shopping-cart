78e06c5c5a0aa425a9c9812b0de38da9cf35d88a
Renata Domingues <blackbirdfly.r@gmail.com>
2021-10-27 13:29:14 -0300

    Requisito 3 graças a deus um requisito facil

--- script.js
+++ script.js
@@ -26,16 +26,15 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// async function cartItemClickListener(event) {
-//   coloque seu código aqui
-  
-// }
+async function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 async function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
