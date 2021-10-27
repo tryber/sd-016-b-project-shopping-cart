802502a86e5d4b04dd6dbdc8b03dc8f9fe3f16c7
João Pedro Araújo Diesel <84046021+jpdiesel@users.noreply.github.com>
2021-10-26 21:23:26 -0300

    Update fetchItem.js

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,6 +2,7 @@ const fetchItem = (searchedProduct) =>
   fetch(`https://api.mercadolibre.com/items/${searchedProduct}`)
     .then((response) => response.json())
     .catch((error) => error);
+//
 
 if (typeof module !== 'undefined') {
   module.exports = {
