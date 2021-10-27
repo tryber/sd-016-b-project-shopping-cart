a0a499fce85e9bab63103b188165f2ca094f9381
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-10-27 03:17:04 -0300

    adiciona testes para a função fetchItem para o requisito 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,7 +4,31 @@ const item = require('../mocks/item');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('2 - Teste a função fecthItem', () => {
+describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Verifica se a função fetchItem com o parâmetro "MLB1615760527" foi chamada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica o endpoint ao chamar a função fetchItem com parâmetro o "MLB1615760527"', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('Verifica se o retorno da função fetchItem com o parâmetro "MLB1615760527" é igual ao objeto item', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+
+  it('Verifica se retorna um erro ao chamar fetchItem sem parâmetro', async () => {
+    const expectError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectError);
+  });
 });
