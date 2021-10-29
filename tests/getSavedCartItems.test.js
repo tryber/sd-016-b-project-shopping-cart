327878be7f29ad759cd88ed374027221319ac930
vinisavordelli <vinicius.savordelli@gmail.com>
2021-10-29 14:40:20 -0300

    testes das funcoes getsaved... e saveCart...

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -10,4 +10,13 @@ describe('4 - Teste a função getSavedCartItems', () => {
   it(('Testa se getSavedCartItems é uma função'), () => {
     expect(typeof getSavedCartItems).toEqual('function');
   });
+
+  it('Verifica se quando a função getSavedCartItems é executada, localStorage.setItem também é chamado', () => {
+      getSavedCartItems();
+      expect(localStorage.getItem).toBeCalled();
+    });
+  
+  it('Verifica se a função getSavedCartItems é chamada com o parâmetro cartItems', () => {
+      expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+    });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -10,4 +10,13 @@ describe('4 - Teste a função saveCartItems', () => {
   it(('Testa se saveCartItems é uma função'), () => {
     expect(typeof saveCartItems).toEqual('function');
   });
+
+  it('Verificar quando a função getSavedCartItems é executada o localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+
+  it('Verificar quando a função saveCartItems com o argumento <ol><li>Item</li></ol> o método localStorage.setItem é chamado', () => {
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
