d2976be504e68bd1c0bf587de96a08758993ef1b
Joey <joviavelino@gmail.com>
2021-10-30 17:01:12 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const getCartItems = document.querySelector('.cart__items');
 const getItems = document.querySelector('.items');
+const getCart = document.querySelector('.cart');
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,12 +29,27 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+const countPrice = async () => {
+  const cartItems = await getCartItems.childNodes;
+  const getTotal = document.querySelector('.total-price');
+  if (cartItems.length === 0) {
+    getTotal.innerText = 0;
+  }
+  const arrayItems = [];
+  for (let index = 0; index < cartItems.length; index += 1) {
+    arrayItems.push(cartItems[1].innerText.split(' '));
+  }
+  const arrayPrices = arrayItems.map((item) => (item[item.length - 1]).substring(1));
+  const arrayNumbers = arrayPrices.map((number) => parseFloat(number));
+  const count = arrayNumbers.reduce((acc, number) => acc + number);
+  getTotal.innerText = count;
+};
 
 function cartItemClickListener(event) {
   const item = event.target;
   item.remove();
-
   saveCartItems();
+  countPrice();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -62,18 +78,27 @@ const itemObject = {
 };
 const element = createCartItemElement(itemObject);
 getCartItems.appendChild(element);
-
 saveCartItems();
+countPrice();
 };
 const addCartElement = (event) => {
   const item = event.target;
   const id = item.parentNode.firstChild.innerText;
   addElement(id);
 };
+const insetPrice = () => {
+  const createPrice = document.createElement('price');
+  createPrice.className = 'total-price';
+  createPrice.innerText = 0;
+  getCart.appendChild(createPrice);
+};
   
   window.onload = () => {
     elementItem();
     getCartItems.addEventListener('click', cartItemClickListener);
     getItems.addEventListener('click', addCartElement);
+    getCartItems.addEventListener('change', countPrice);
     getSavedCartItems();
+    insetPrice();
+    countPrice();
  };
