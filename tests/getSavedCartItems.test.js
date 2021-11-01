f742c5a1d5440602876373c146fd08374ceec638
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-11-01 12:15:07 -0300

    solves tests of requirement 4.

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,4 +1,5 @@
 const getSavedCartItems = require('../helpers/getSavedCartItems');
+const saveCartItems = require('../helpers/saveCartItems');
 
 Object.defineProperty(window, 'localStorage', {
   value: {
@@ -8,5 +9,13 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('tests if when executing "getSavedCartItems" "localStorage.setItem" is called', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+  it('tests if when executing "getSavedCartItems" "localStorage.setItem" is called with "cartItem" as param', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,13 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('test if when "saveCartItems" is called whith param "<ol><li>Item</li></ol>" "LocalStorage.setItem" method is called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('test if when "saveCartItems" is called whith param "<ol><li>Item</li></ol>" "LocalStorage.setItem" method is called whit two params', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
