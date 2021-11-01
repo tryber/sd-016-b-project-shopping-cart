4859edc3e615715afe1d985b449f0b435ddfb1d7
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-11-01 20:48:06 -0300

    adiciona testes para a função saveCartItems - requisito 4

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,15 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se o método localStorage.setItem foi chamado ao chamar a função saveCartItems com o parâmetro "<ol><li>Item</li></ol>"', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verifica se o método localStorage.setItem foi chamado com dois parâmetros ao chamar a função saveCartItems com o parâmetro "<ol><li>Item</li></ol>"', () => {
+    const param1 = 'cartItems';
+    const param2 = '<ol><li>Item</li></ol>'
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(param1, param2);
+  });
 });
