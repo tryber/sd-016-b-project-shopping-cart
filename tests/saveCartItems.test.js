48d8e12f1744703b4861a15b7c2f79f8155caf38
calidor <victor.calidor@gmail.com>
2021-11-01 03:50:27 -0300

    ACABOU

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,7 +7,14 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  it('is it a function', () => {
-    expect(typeof saveCartItems).toBe('function');
-  });
+  
+  const param = '<ol><li>Item</li></ol>'
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
+    saveCartItems(param)
+    expect(localStorage.setItem).toHaveBeenCalled()
+  })
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
+    saveCartItems(param)
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>')
+  })
 });
