3f612406cd523ec349a5336875f3d314b224cfb2
Felipe Pugas <felipepcan@gmail.com>
2021-11-01 12:07:31 -0300

    teste 2

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -104,7 +116,7 @@ function emptyCart() {
   while (ol.firstChild) {
     ol.removeChild(ol.firstChild);
   }
-  document.querySelector('.price').innerText = 0;
+  /* document.querySelector('.price').innerText = 0; */
 }
 
 function emptyShoppingCart() {
