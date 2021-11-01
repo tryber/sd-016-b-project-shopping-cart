0c5d226aabdedf63af2f89a5c8bc6dd2687f3882
Marcus Thadeu <marcusthadeu@outlook.com>
2021-11-01 11:10:18 -0300

    lint correct

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,23 +1,15 @@
-const saveCartItems = require('../helpers/saveCartItems');
-
-Object.defineProperty(window, 'localStorage', {
-  value: {
-    setItem: jest.fn(),
-  },
+describe('4 - Teste a função saveCartItems', () => {
+it('1.1 - Verificar se o saveCartItems.js é realmente uma Function:', () => {
+  expect(typeof saveCartItems).toEqual('function');
 });
-
-  describe('3 - Teste a função saveCartItems', () => {
-    it('Deve chamar o método localStorage.setItem', () => {
-      const cartItems = '<ol><li>Item</li></ol>';
-  
-      saveCartItems(cartItems);
-      expect(localStorage.setItem).toHaveBeenCalled();
-    });
-  
-    it('Deve chamar o método localStorage.setItem com o argumento correto', () => {
-      const cartItems = '<ol><li>Item</li></ol>';
-  
-      saveCartItems(cartItems);
-      expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', cartItems);
-    });
+it('1.2 - Verificar se o saveCartItems.js é realmente uma Function with InstanceOf:',() => {
+  expect(saveCartItems).toBeInstanceOf(Function);
+}); it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+  saveCartItems('<ol><li>Item</li></ol>');
+  expect(localStorage.setItem).toHaveBeenCalled();
+});
+it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para', () => {
+  saveCartItems('<ol><li>Item</li></ol>');
+  expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+});
 });
