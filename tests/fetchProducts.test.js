0b96bb128e90fa7cc489f261a1fb00266aa26b84
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-11-01 11:38:57 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,24 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se é função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Testa se a função é chamada', () => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Testa se a função chamada com argumento ´computador´ utiliza o endpoint ´https://api.mercadolibre.com/sites/MLB/search?q=computador´', () => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('Testa se a função chamada com argumento ´computador´ retorna um objeto igual ao armazenado na constante computadorSearch', async () => {
+    const resultExpected = await fetchProducts('computador');
+    expect(resultExpected).toEqual(computadorSearch);
+  });
+  it('Testa se a função chamada sem argumento retorna um erro', async () => {
+    const erroEsperado = new Error('You must provide an url');
+    const resultExpected = await fetchProducts();
+    expect(resultExpected).toEqual(erroEsperado);
+  });
 });
