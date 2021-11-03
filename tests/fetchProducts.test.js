1af9776b06818099fd959fce9f5068f56acdfdaa
lfpborba <luisfpb@icloud.com>
2021-11-02 21:32:52 -0300

    Requisito 1 testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,29 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1.1 - Deve ser uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it("1.2 - Ao chamar o argumento computador, testa se fetch foi chamada", () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('1.3 - Ao chamá-la com o argumento computador, testa se fetch foi chamada com o endpoint correto', () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it("1.4 - Testa se o retorno da função é um objeto igual a computadorSearch", async () => {
+    const results = await fetchProducts('computador');
+    expect(results).toEqual(computadorSearch);
+  });
+
+  it("1.5 - Deve retornar um erro", async () => {
+    const expectedError = new Error('You must provide an url');
+    const result = await fetchProducts();
+    expect(result).toEqual(expectedError)
+  });
 });
