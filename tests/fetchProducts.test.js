409782e77b05d18c3ad38b7720a9e683cd23cd69
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-11-06 22:15:02 -0300

    Corrigindo erro-02

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -13,7 +13,18 @@ describe('1 - Teste a função fecthProducts', () => {
     fetchProducts('computador');  
     expect(fetch).toHaveBeenCalled();
   });
+
+  it('Ao chamar a função fetchProducts com o argumento "computador" ela utiliza o endpoint correto', () => {
+    fetchProducts('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
   
+  it('O retorno da função é um objeto igual a computadorSearch', async () => {
+    const results = await fetchProducts('computador');
+    expect(results).toEqual(computadorSearch);
+  });
+
   it('se o parâmetro de fetchProducts for indefinido, devolver erro', async () => {
     const error = new Error('You must provide an url')
     const test = await fetchProducts();
