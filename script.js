425d8788cfee2749a5d6d5f6aa20f47354840024
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-11-08 10:59:14 -0300

    Requisito-03

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
