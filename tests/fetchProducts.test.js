8fd92a6a7e0e71455607ce0da8f4960749bb7d5b
Marcus Thadeu <marcusthadeu@outlook.com>
2021-11-08 16:41:07 -0300

    90% complete

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -14,4 +14,11 @@ describe('1 - Teste a função fetchProducts', () => {
     expect.assertions(1);
     expect(fetchProducts).toBeInstanceOf(Function);
   });
+  
+  it('Testa se ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint correto', async () => {
+  expect.assertions(1);
+  const correctEndpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalledWith(correctEndpoint);
+  });
 });
