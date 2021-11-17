97e4dd8d8ce38e77820f672aba9e3bf020b7841c
Istvan <olivaistvan@gmail.com>
2021-11-17 02:13:24 -0300

    Requisito 01

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (item) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+    fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 };
+
+// console.log(fetchProducts('computador'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
