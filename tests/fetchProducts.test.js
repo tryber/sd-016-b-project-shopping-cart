648c6b05591b37772f43d7a442a8f5b87438757c
Savio Silva <s.drksilva@gmail.com>
2021-11-19 02:53:59 -0300

    Added tests to coverage the function fetchProducts()

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,26 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1.1 - Teste se fetchProducts é uma função;', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('1.2 - Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('1.3 - utiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('1.4 - estrutura de dados igual ao objeto computadorSearch', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('1.5 - sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const result = await fetchProducts();
+    expect(result).toEqual(new Error('You must provide an url'));
+  });
 });
