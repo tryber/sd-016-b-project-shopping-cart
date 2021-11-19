82f3246890192e48c20407ddbceb4f012afeaeb6
Savio Silva <s.drksilva@gmail.com>
2021-11-19 03:02:45 -0300

    Added tests to coverage the function fetchItem()

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,5 @@
-const fetchItem = (item) => fetch(`"https://api.mercadolibre.com/items/${item}"`)
+const fetchItem = (item) =>
+  fetch(`https://api.mercadolibre.com/items/${item}`)
   .then((response) => response.json())
   .then((data) => data)
   .catch((error) => error);
