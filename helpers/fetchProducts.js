d7ce113cd654d1431baa51dac5a7b647b01ee098
Douglas Sabino <douglasbsabino@hotmail.com>
2021-11-19 17:58:24 -0300

    termina fetchProdutos/item e getsavedCartitems

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,6 +2,7 @@ const fetchProducts = (productName) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productName}`)
   .then((response) => response.json())
   .then((data) => data)
+  .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {
