564f324c906691b170dde5f60608ec1de911f856
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-11-22 03:16:10 -0300

    Completa os testes do requisito 1 e 4

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,22 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Verifica se a função fetchProducts com o parâmetro `computador` chama a `fetch`', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se a função fetchProducts com o parâmetro `computador` usa o endpoint correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Verifica se a função fetchProducts com o parâmetro `computador` retorna o objeto correto', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toBe(computadorSearch);
+  });
 });
