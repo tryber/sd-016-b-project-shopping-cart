3c9a78567545d6cd9ec3aeb7b311d97a7f930c3e
guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2021-11-23 09:57:57 -0300

    primeira parte

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,5 +6,27 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Verifica se ao chamar fetchItem com o argumento MLB1615760527 a função fetch é chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+
+  it('Verifica se ao chamar fetchItem com o argumento MLB1615760527 a função fetch utiliza o endpoint correto', () => {
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
+
+  it('Verifica se retorna o objeto corretamente ao chamar com o argumento MLB1615760527', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  })
+
+  it('Verifica se ao chamar a função sem parâmetros retorna um erro', async () => {
+    const getFetchItem = await fetchItem();
+    const newError = new Error('You must provide an url');
+
+    expect(getFetchItem).toEqual(newError);
+  })
 });
